c3c176429e674db1f1648358151ca914ed4093c4
Sam Skjonsberg <sams@allenai.org>
2023-09-11 08:49:43 -0700

    Increase default for `max_tokens`.
    This increases the default to 2048, while also increasing the max.
    LLaMa2's context window is 4096, which is why I chose that #. That
    said I'm not sure if that makes sense or not. In theory:
    
    - the context window should dictate the size of the inputs/prompt;
      anything that overflows 4096 tokens presumably needs to be
      compressed/summarized
    
    - the output maximum is a function of how much GPU memory is available
      after prefilling the input tokens; which means the maximum value is
      again a function of the input length.
    
    We'll do something smarter later, for now this should prevent the model
    from being "cut off" as often.

--- src/dao/message.py
+++ src/dao/message.py
@@ -21,7 +21,7 @@ class Field:
     max: Any
     step: Optional[int | float] = None
 
-max_tokens = Field("max_tokens", 256, 1, 2048, 1)
+max_tokens = Field("max_tokens", 2048, 1, 4096, 1)
 temperature = Field("temperature", 1.0, 0.0, 2.0, 0.01)
 num = Field("n", 1, 1, 50, 1)
 top_p = Field("top_p", 1.0, 0.0, 1.0, 0.01)

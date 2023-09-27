6941cc8227cd4ff153bdbc850736832367fe5dd9
Sam Skjonsberg <sams@allenai.org>
2023-09-27 10:04:07 -0700

    Better elasticsearch error handling (#36)
    This modifies our error handling so that we gracefully handle errors
    from Elasticsearch. Elasticsearch conveniently provides a HTTP status
    code we can use to avoid serving a 500 in all scenarios. They do not,
    however, make it easy to produce a friendly error message.
    
    For now I figured we'll opt towards verbosity/detail. Over the long run
    we can whittle this down to an exhaustive set of errors that are more
    user friendly.
    
    See https://github.com/allenai/olmo-api/issues/30

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ def create_app():
         return "", 204
 
     app.register_blueprint(v3.Server(dbc, inferd, didx, cfg), url_prefix="/v3", name="v3")
-    app.register_error_handler(HTTPException, error.handle)
+    app.register_error_handler(HTTPException, error.handle_http)
+    app.register_error_handler(es8.ApiError, error.handle_es)
 
     ProxyFix(app, x_for=cfg.server.num_proxies, x_proto=cfg.server.num_proxies,
              x_host=cfg.server.num_proxies, x_port=cfg.server.num_proxies)
--- src/error.py
+++ src/error.py
@@ -2,17 +2,41 @@ from flask import jsonify, request, render_template, make_response
 from flask.typing import ResponseReturnValue
 from werkzeug.exceptions import HTTPException
 from werkzeug import http
+from typing import Optional
+from logging import getLogger
 
-def handle(e: HTTPException) -> ResponseReturnValue:
+import elasticsearch8 as es8
+
+def make_error_response(code: Optional[int] = None, message: Optional[str] = None) -> ResponseReturnValue:
+    if code is None:
+        code = 500
+    if message is None:
+        message = "Internal Server Error"
     accept = http.parse_accept_header(request.headers.get("Accept", "application/json"))
     match accept.best:
         case "text/html":
-            r = make_response(render_template("error.jinja", title=f"HTTP {e.code}", message=e.description))
-            r.status = e.code if e.code is not None else 500
-            return r
+            return make_response(render_template("error.jinja", title=f"HTTP {code}", message=message), code)
         case _:
-            code = e.code if e.code is not None else 500
-            r = jsonify({ "error": { "code": code, "message": e.description }})
-            r.status = code
-            return r
+            return make_response(jsonify({ "error": { "code": code, "message": message }}), code)
+
+def handle_http(e: HTTPException) -> ResponseReturnValue:
+    getLogger(__name__).exception(e)
+    return make_error_response(e.code, e.description)
+
+def handle_es(e: es8.exceptions.ApiError) -> ResponseReturnValue:
+    getLogger(__name__).exception(e)
+
+    # Do our best to unpack a meaningful error.
+    # TODO: manifest friendlier (and safer) errors via exhaustive pattern matching
+    details = []
+    causes = e.body.get("error", {}).get("root_cause", [])
+    for cause in causes:
+        reason = cause.get("reason")
+        if reason is not None:
+            details.append(reason)
+
+    # e.message isn't useful for the client, but we'll include it right now, as we currently
+    # opt for verbosity over user-friendliness
+    message = f"{e.message}: {', '.join(details)}"
+    return make_error_response(e.status_code, message)
 

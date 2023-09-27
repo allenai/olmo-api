38eb884700d46ec248d4af274f3996f039bf92f9
Sam Skjonsberg <sams@allenai.org>
2023-09-26 18:37:25 -0700

    Friendly errors
    Add a friendly error page and some custom error messages
    for the invite flow.
    
    This is very rough/generic at this point, but better than
    nothing.

--- src/error.py
+++ src/error.py
@@ -1,10 +1,18 @@
-from flask import jsonify
+from flask import jsonify, request, render_template, make_response
 from flask.typing import ResponseReturnValue
 from werkzeug.exceptions import HTTPException
+from werkzeug import http
 
 def handle(e: HTTPException) -> ResponseReturnValue:
-    code = e.code if e.code is not None else 500
-    r = jsonify({ "error": { "code": code, "message": e.description }})
-    r.status = code
-    return r
+    accept = http.parse_accept_header(request.headers.get("Accept", "application/json"))
+    match accept.best:
+        case "text/html":
+            r = make_response(render_template("error.jinja", title=f"HTTP {e.code}", message=e.description))
+            r.status = e.code if e.code is not None else 500
+            return r
+        case _:
+            code = e.code if e.code is not None else 500
+            r = jsonify({ "error": { "code": code, "message": e.description }})
+            r.status = code
+            return r
 

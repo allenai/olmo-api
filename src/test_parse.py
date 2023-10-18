d30b0f9cdbcb5fd7b895b7e75e3a9ba107e5dfd1
Sam Skjonsberg <sams@allenai.org>
2023-10-17 22:35:04 -0700

    Support for finding messages
    This is the first step towards replacing datachips in new message
    content. It adds code for parsing HTML (using `BeautifulSoup`) and
    finding and replacing datachips.
    
    The code isn't called yet by the API. Instead I elected to add a few
    tests. After this lands I'll connect things, but it felt right to review
    each change in isolation.

--- src/test_parse.py
+++ src/test_parse.py
@@ -1,5 +1,6 @@
 from . import parse
 from datetime import timedelta
+from dataclasses import dataclass
 
 import pytest
 
@@ -31,3 +32,66 @@ def test_parse_invalid_timedelta():
     for input in tests:
         with pytest.raises(ValueError):
             parse.timedelta_from_str(input)
+
+@dataclass
+class DatachipTestCase:
+    name: str
+    input: str
+    expected_output: str
+    chips: dict[str, str]
+
+def test_parse_and_replace_datachips():
+    tests = [
+        # A typical case with several well-formed datachips
+        DatachipTestCase(
+            "typical",
+            """
+<h1>Hey <span data-datachip-id="to">To</span>,</h1>
+<p>My name is <span data-datachip-id="from">From</span>.</p>
+<p>Do you like to <strong><span data-datachip-id="activity">Activity</span>?</strong></p>
+Best,<br>
+<span data-datachip-id="from">From</span>
+            """.strip(),
+            """
+<h1>Hey Murphy,</h1>
+<p>My name is Logan.</p>
+<p>Do you like to <strong>dig holes?</strong></p>
+Best,<br>
+Logan
+            """.strip(),
+            { "to": "Murphy", "from": "Logan", "activity": "dig holes" }
+        ),
+        # A datachip with a child tag
+        DatachipTestCase(
+            "nested_marquee",
+            """Hi <span data-datachip-id="name"><marquee>Name</marquee></span>!""",
+            """Hi Murphy!""",
+            { "name": "Murphy" }
+        ),
+        # Recursive, nested datachips
+        DatachipTestCase(
+            "nested_chip",
+            """Hi <span data-datachip-id="name1"><span data-datachip-id="name2">Name</span></span>!""",
+            """Hi Murphy!""",
+            { "name1": "Murphy", "name2": "Logan" }
+        ),
+        # Emojis
+        DatachipTestCase(
+            "emoji",
+            """👋 <span data-datachip-id="name">Name</span>""",
+            """👋 Murphy""",
+            { "name": "Murphy" }
+        ),
+        # HTML Entities
+        DatachipTestCase(
+            "emoji",
+            """Hi &ldquo;<span data-datachip-id="name">Name</span>&rdquo;""",
+            """Hi &ldquo;Murphy&rdquo;""",
+            { "name": "Murphy" }
+        ),
+    ]
+    for test in tests:
+        content = parse.MessageContent(test.input)
+        for dc in content.datachips():
+            dc.tag.replace_with(test.chips[dc.id()])
+        assert content.html() == test.expected_output, f"Failed to parse datachips for test {test.name}"

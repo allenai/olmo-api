f51eb9184588d3468634cb7cc03f8474e5536c17
michael wilson <michaelw@allenai.org>
2024-02-01 12:59:52 -0800

    Support empty token_ids returned from model (#89)
    The OLMo 7B model does not currently return `token_ids`, which caused an
    error:
    ```py
    Traceback (most recent call last):
      File "/usr/local/lib/python3.11/site-packages/werkzeug/serving.py", line 362, in run_wsgi
        execute(self.server.app)
      File "/usr/local/lib/python3.11/site-packages/werkzeug/serving.py", line 325, in execute
        for data in application_iter:
      File "/usr/local/lib/python3.11/site-packages/werkzeug/wsgi.py", line 256, in __next__
        return self._next()
               ^^^^^^^^^^^^
      File "/usr/local/lib/python3.11/site-packages/werkzeug/wrappers/response.py", line 32, in _iter_encoded
        for item in iterable:
      File "/api/src/v3.py", line 439, in stream
        part = OutputPart.from_struct(resp.result.output)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/api/src/v3.py", line 60, in from_struct
        return cls(op["text"], [int(tid) for tid in op["token_ids"]], logprobs, fr)
                                                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    KeyError: 'token_ids'
    ```
    
    This PR makes them not required, as we are not storing them.

--- src/v3.py
+++ src/v3.py
@@ -42,11 +42,13 @@ class OutputPart:
     """
     An OutputPart is a single message part delivered from the Tulu2 model:
     https://github.com/allenai/inferd-tulu2/blob/main/src/svllm.py#L16-L20
+    We also support the OLMo 7B model which currently only returns text:
+    https://github.com/allenai/inferd-olmo/blob/b2aec55d942aee0a894fd080da50deed9e1f8440/src/model.py#L47-L47
 
     If we end up using other models via the same UI, we'll need to generalize.
     """
     text: str
-    token_ids: list[int]
+    token_ids: Optional[list[int]]
     logprobs: Optional[list[list[message.TokenLogProbs]]] = None
     finish_reason: Optional[FinishReason] = None
 
@@ -57,7 +59,8 @@ class OutputPart:
             [ message.TokenLogProbs(int(lp["token_id"]), lp["text"], lp["logprob"]) for lp in lps ] for lps in op["logprobs"]
         ] if op.get("logprobs") is not None else None
         fr = FinishReason(op["finish_reason"]) if op.get("finish_reason") is not None else None
-        return cls(op["text"], [int(tid) for tid in op["token_ids"]], logprobs, fr)
+        token_ids = [int(tid) for tid in op["token_ids"]] if op.get("token_ids") is not None else None
+        return cls(op["text"], token_ids, logprobs, fr)
 
 @dataclasses.dataclass
 class AuthenticatedClient:

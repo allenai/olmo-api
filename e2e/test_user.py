2fcab4ead87ea8d4a2e400dcfaba48b547ee6c3b
Taylor Blanton <matthewtblanton@gmail.com>
2024-04-23 12:58:51 -0500

    add test for new T&C logic

--- e2e/test_user.py
+++ e2e/test_user.py
@@ -31,6 +31,11 @@ class TestUserEndpoints(base.IntegrationTest):
             == terms_accepted_date.astimezone(timezone.utc).isoformat()
         )
 
+        # whoami should return that the user has accepted T&Cs
+        r = requests.get(url=f"{self.origin}/v3/whoami", cookies={"token": user.token})
+        payload = r.json()
+        assert payload["hasAcceptedTermsAndConditions"] is True
+
         acceptance_revoked_date = datetime.now(ZoneInfo("America/Los_Angeles"))
         # Happy path: Updates an existing user
         r = requests.put(
@@ -52,3 +57,33 @@ class TestUserEndpoints(base.IntegrationTest):
             payload["acceptanceRevokedDate"]
             == acceptance_revoked_date.astimezone(timezone.utc).isoformat()
         )
+
+        # whoami should return that the user has not accepted T&Cs if they've revoked
+        r = requests.get(url=f"{self.origin}/v3/whoami", cookies={"token": user.token})
+        payload = r.json()
+        assert payload["hasAcceptedTermsAndConditions"] is False
+
+        new_terms_accepted_date = datetime.now(ZoneInfo("America/Los_Angeles"))
+
+        # Happy path: Creates user if it doesn't exist
+        r = requests.put(
+            f"{self.origin}/v3/user",
+            cookies={"token": user.token},
+            json={"termsAcceptedDate": new_terms_accepted_date.isoformat()},
+        )
+        r.raise_for_status()
+
+        payload = r.json()
+        assert (
+            payload["termsAcceptedDate"]
+            == new_terms_accepted_date.astimezone(timezone.utc).isoformat()
+        )
+        assert (
+            payload["acceptanceRevokedDate"]
+            == acceptance_revoked_date.astimezone(timezone.utc).isoformat()
+        )
+
+        # whoami should return that the user has accepted T&Cs if they've revoked acceptance then accepted again later
+        r = requests.get(url=f"{self.origin}/v3/whoami", cookies={"token": user.token})
+        payload = r.json()
+        assert payload["hasAcceptedTermsAndConditions"] is True

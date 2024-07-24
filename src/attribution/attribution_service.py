002c1f3c68a1d61275f689e45a9f9e982d05a30d
Taylor Blanton <matthewtblanton@gmail.com>
2024-07-24 13:30:13 -0500

    wip attribution service

--- src/attribution/attribution_service.py
+++ src/attribution/attribution_service.py
@@ -9,12 +9,18 @@ from src.attribution.infini_gram_api_client.models.attribution_request import (
 from src.attribution.infini_gram_api_client.models.available_infini_gram_index_id import (
     AvailableInfiniGramIndexId,
 )
+from src.attribution.infini_gram_api_client.models.http_validation_error import (
+    HTTPValidationError,
+)
+from src.attribution.infini_gram_api_client.models.infini_gram_attribution_response_with_docs import (
+    InfiniGramAttributionResponseWithDocs,
+)
 from src.config import cfg
 
 
 def get_attribution(model_response: str, model_id: str):
     infini_gram_client = Client(base_url=cfg.infini_gram_api_url)
-    # TODO: get the correct index
+    # TODO: get the correct index for the model
     attribution_response = get_document_attributions_index_attribution_post.sync(
         index=AvailableInfiniGramIndexId.DOLMA_1_7,
         client=infini_gram_client,
@@ -22,3 +28,13 @@ def get_attribution(model_response: str, model_id: str):
             query=model_response, include_documents=True, minimum_span_length=10
         ),
     )
+
+    if isinstance(attribution_response, HTTPValidationError):
+        # validation error handling
+        ...
+
+    if isinstance(attribution_response, InfiniGramAttributionResponseWithDocs):
+        documents = []
+        for span in attribution_response.spans:
+            for document in span.documents:
+                documents.append(document)

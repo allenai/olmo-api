14413b0f37417ca933d45f82ec494b73ae528871
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2024-09-26 11:26:02 -0700

    Make sure default model will be the first one in the list of that modal (#144)
    Ref: https://allenai.atlassian.net/browse/OEUI-190
    While adding the new Peteish model into the local `config.json `to make
    sure it works i noticed that despite in the config i set the
    `default_model` to be it but when the list renders on UI it still at the
    last position. I made a tweak into to make sure for each `host` we will
    want the `default_element` to be at the top instead of whatever the
    position they are currently on for each `host`. I verified the model
    work by submitting a question.
    ## Screenshot/Video 
    
    ![image](https://github.com/user-attachments/assets/fe524d05-5081-4349-9b94-d46ac913a938)
    
    ![image](https://github.com/user-attachments/assets/5e14527c-976e-4446-8d4a-8b81288ffe06)
    
    
    https://github.com/user-attachments/assets/d62fb7c9-309c-4571-a8e4-fffd103692e2

--- src/inference/inference_service.py
+++ src/inference/inference_service.py
@@ -16,17 +16,28 @@ class ModelEntity:
 
 def get_available_models() -> Sequence[ModelEntity]:
     all_models = []
+
     for host in config.model_hosts:
-        for model in getattr(config.cfg, host).available_models:
-            all_models.append(
-                ModelEntity(
-                    id=model.id,
-                    host=host,
-                    name=model.name,
-                    description=model.description,
-                    model_type=model.model_type,
-                    is_deprecated=model.is_deprecated or False,
-                )
+        host_config = getattr(config.cfg, host)
+        default_model = host_config.default_model
+        models_for_host = []
+
+        for model in host_config.available_models:
+            model_entity = ModelEntity(
+                id=model.id,
+                host=host,
+                name=model.name,
+                description=model.description,
+                model_type=model.model_type,
+                is_deprecated=model.is_deprecated or False,
             )
 
+            # Add the default model first, and other models afterward
+            if model.id == default_model:
+                models_for_host.insert(0, model_entity)
+            else:
+                models_for_host.append(model_entity)
+
+        # Now add all models for the current host (with the default first) to the main list
+        all_models.extend(models_for_host)
     return all_models

67abe4d6e2b1b5a704505ebf70800cf464ed9177
Chris Newell <cnewell+git@gmail.com>
2024-10-10 14:49:49 -0700

    Switching wildguard hosting from inferd to modal (#149)

--- src/message/WildGuard.py
+++ src/message/WildGuard.py
@@ -1,7 +1,7 @@
 import dataclasses
 from typing import Optional
 
-from inferd import Client as InferdClient
+import modal
 
 from src import config
 
@@ -20,26 +20,25 @@ class WildguardResponse:
 
 
 class WildGuard:
-    inferDClient: InferdClient
-    computeSourceId: str
+    modalClient: modal.Client
 
     def __init__(self) -> None:
-        self.inferDClient = InferdClient(
-            config.cfg.wildguard.address, config.cfg.wildguard.token
-        )
-        self.computeSourceId = config.cfg.wildguard.compute_source_id
+        self.modalClient  = modal.Client.from_credentials(
+            config.cfg.modal.token, config.cfg.modal.token_secret)
+
 
     def check_request(self, request: WildguardRequest) -> WildguardResponse:
-        payload = dataclasses.asdict(request)
+        f = modal.Function.lookup("wildguard", "wildguard_api", client=self.modalClient)
 
         # the wildguard returns a generator that yields a single response
-        result = list(self.inferDClient.infer(self.computeSourceId, payload)).pop()
+        result = list(f.remote_gen(request.prompt)).pop()
 
         return WildguardResponse(
             request_harmful=self.transform_value(result["request_harmful"]),
             response_refusal=self.transform_value(result["response_refusal"]),
             response_harmful=self.transform_value(result["response_harmful"]),
         )
+
 
     def transform_value(self, val: str):
         if val == "yes":

384d7bc44f643b747f00dcb683fd7e773db17746
Taylor Blanton <matthewtblanton@gmail.com>
2024-10-16 15:00:45 -0500

    [OEUI-138] add all snippets to the document response (#148)
    This PR adds every snippet in a document that contains a span to the
    documents in the attribution response

--- src/attribution/attribution_service.py
+++ src/attribution/attribution_service.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List, Optional, cast
+from typing import List, Optional, Self, cast
 
 from flask import json, request
 from werkzeug import exceptions
@@ -22,13 +22,20 @@ from src.attribution.infini_gram_api_client.models.http_validation_error import 
 )
 from src.config import cfg
 
-from .flatten_spans import flatten_spans
+from .flatten_spans import FlattenedSpan, FlattenedSpanDocument, flatten_spans
 from .infini_gram_api_client import Client
+
+
+@dataclass
+class AttributionDocumentSnippet:
+    text: str
+    corresponding_span_text: str
 
 
 @dataclass
 class ResponseAttributionDocument:
     text: str
+    snippets: list[AttributionDocumentSnippet]
     corresponding_spans: List[int]
     corresponding_span_texts: List[str]
     index: str
@@ -36,6 +43,51 @@ class ResponseAttributionDocument:
     relevance_score: float
     title: Optional[str] = None
     url: Optional[str] = None
+
+    @classmethod
+    def from_flattened_span_document(
+        cls, document: FlattenedSpanDocument, span_index: int
+    ) -> Self:
+        metadata = document.metadata.additional_properties.get("metadata", {})
+        if "metadata" in metadata:
+            url = metadata["metadata"].get("url", None)
+        elif "doc" in metadata:
+            url = metadata["doc"].get("url", None)
+        else:
+            url = None
+
+        source = document.metadata.additional_properties.get("path", "").split("/")[0]
+        if source not in [
+            "arxiv",
+            "algebraic-stack",
+            "open-web-math",
+            "pes2o",
+            "starcoder",
+            "wiki",
+        ]:
+            source = metadata.get("source", None)
+            if source == "dclm-hero-run-fasttext_for_HF":
+                source = "dclm"
+
+        return cls(
+            text=document.text,
+            snippets=[
+                AttributionDocumentSnippet(
+                    text=document.text, corresponding_span_text=document.span_text
+                )
+            ],
+            corresponding_spans=[span_index],
+            corresponding_span_texts=[document.span_text],
+            index=str(document.document_index),
+            source=source,
+            title=document.metadata.additional_properties.get("metadata", {})
+            .get("metadata", {})
+            .get("title", None),
+            url=url,
+            relevance_score=(
+                document.relevance_score if document.relevance_score is not None else 0
+            ),
+        )
 
 
 class GetAttributionRequest(APIInterface):
@@ -58,6 +110,43 @@ class ResponseAttributionSpan:
 @dataclass
 class TopLevelAttributionSpan(ResponseAttributionSpan):
     nested_spans: List[ResponseAttributionSpan] = field(default_factory=lambda: [])
+
+    @classmethod
+    def from_flattened_span(cls, span: FlattenedSpan) -> Self:
+        return cls(
+            text=span.text,
+            nested_spans=[
+                ResponseAttributionSpan(
+                    text=nested_span.text,
+                    documents=[
+                        document.document_index for document in nested_span.documents
+                    ],
+                )
+                for nested_span in span.nested_spans
+            ],
+        )
+
+
+def update_mapped_document(
+    mapped_document: ResponseAttributionDocument,
+    span_index: int,
+    span_text: str,
+    new_document: FlattenedSpanDocument,
+):
+    if span_index not in mapped_document.corresponding_spans:
+        mapped_document.corresponding_spans.append(span_index)
+
+    if span_text not in mapped_document.corresponding_span_texts:
+        mapped_document.corresponding_span_texts.append(span_text)
+
+    if not any(
+        snippet.text == new_document.text for snippet in mapped_document.snippets
+    ):
+        mapped_document.snippets.append(
+            AttributionDocumentSnippet(
+                text=new_document.text, corresponding_span_text=new_document.span_text
+            )
+        )
 
 
 def get_attribution(
@@ -96,97 +185,53 @@ def get_attribution(
             description="The version of infinigram-api we hit doesn't support or didn't return input_tokens"
         )
 
-    collapsed_spans = flatten_spans(
+    flattened_spans = flatten_spans(
         input_tokens=attribution_response.input_tokens,
         spans=cast(List[AttributionSpanWithDocuments], attribution_response.spans),
     )
 
-    documents: dict[int, ResponseAttributionDocument] = {}
-    spans: dict[int, ResponseAttributionSpan] = {}
-    for span_index, span in enumerate(collapsed_spans):
-        if spans.get(span_index) is None:
-            spans[span_index] = TopLevelAttributionSpan(
-                text=span.text,
-                nested_spans=[
-                    ResponseAttributionSpan(
-                        text=nested_span.text,
-                        documents=[
-                            document.document_index
-                            for document in nested_span.documents
-                        ],
+    mapped_documents: dict[int, ResponseAttributionDocument] = {}
+    mapped_spans: dict[int, ResponseAttributionSpan] = {}
+
+    for span_index, span in enumerate(flattened_spans):
+        if span_index not in mapped_spans:
+            mapped_spans[span_index] = TopLevelAttributionSpan.from_flattened_span(span)
+
+        for current_span_document in span.documents:
+            if (
+                current_span_document.document_index
+                not in mapped_spans[span_index].documents
+            ):
+                mapped_spans[span_index].documents.append(
+                    current_span_document.document_index
+                )
+
+            if current_span_document.document_index not in mapped_documents:
+                mapped_documents[current_span_document.document_index] = (
+                    ResponseAttributionDocument.from_flattened_span_document(
+                        current_span_document, span_index
                     )
-                    for nested_span in span.nested_spans
-                ],
-            )
-
-        for document in span.documents:
-            if document.document_index not in spans[span_index].documents:
-                spans[span_index].documents.append(document.document_index)
-
-            if documents.get(document.document_index) is None:
-                metadata = document.metadata.additional_properties.get("metadata", {})
-                if "metadata" in metadata:
-                    url = metadata["metadata"].get("url", None)
-                elif "doc" in metadata:
-                    url = metadata["doc"].get("url", None)
-                else:
-                    url = None
-                source = document.metadata.additional_properties.get("path", "").split(
-                    "/"
-                )[0]
-                if source not in [
-                    "arxiv",
-                    "algebraic-stack",
-                    "open-web-math",
-                    "pes2o",
-                    "starcoder",
-                    "wiki",
-                ]:
-                    source = metadata.get("source", None)
-                    if source == "dclm-hero-run-fasttext_for_HF":
-                        source = "dclm"
-
-                documents[document.document_index] = ResponseAttributionDocument(
-                    text=document.text,
-                    corresponding_spans=[span_index],
-                    corresponding_span_texts=[document.span_text],
-                    index=str(document.document_index),
-                    source=source,
-                    title=metadata.get("metadata", {}).get("title", None),
-                    url=url,
-                    relevance_score=(
-                        document.relevance_score
-                        if document.relevance_score is not None
-                        else 0
-                    ),
                 )
             else:
-                if (
-                    span_index
-                    not in documents[document.document_index].corresponding_spans
-                ):
-                    documents[document.document_index].corresponding_spans.append(
-                        span_index
-                    )
+                update_mapped_document(
+                    # We make sure the mapped_document is present in the if corresponding to this else
+                    mapped_documents.get(current_span_document.document_index),  # type: ignore [arg-type]
+                    span_text=span.text,
+                    new_document=current_span_document,
+                    span_index=span_index,
+                )
 
-                if (
-                    document.span_text
-                    not in documents[document.document_index].corresponding_span_texts
-                ):
-                    documents[document.document_index].corresponding_span_texts.append(
-                        document.span_text
-                    )
     if request.spans_and_documents_as_list is True:
         return {
             "documents": sorted(
-                documents.values(),
+                mapped_documents.values(),
                 key=lambda document: document.relevance_score,
                 reverse=True,
             ),
-            "spans": list(spans.values()),
+            "spans": list(mapped_spans.values()),
         }
     else:
-        return {"documents": documents, "spans": spans}
+        return {"documents": mapped_documents, "spans": mapped_spans}
 
 
 def _validate_get_attribution_request():

d2496ff263763645ea19e80278f8ebe618256fd9
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-11 16:25:47 -0600

    show what messages are getting deleted

--- message_deletion_job/delete_expired_messages.sql
+++ message_deletion_job/delete_expired_messages.sql
@@ -1,4 +1,10 @@
-DELETE *
+SELECT id,
+    role,
+    creator
 FROM message
 WHERE expiration_time IS NOT NULL
-    AND expiration_time <= CURRENT_TIMESTAMP AT TIME ZONE 'utc'
+    AND expiration_time <= CURRENT_TIMESTAMP AT TIME ZONE 'utc';
+
+DELETE FROM message
+WHERE expiration_time IS NOT NULL
+    AND expiration_time <= CURRENT_TIMESTAMP AT TIME ZONE 'utc';

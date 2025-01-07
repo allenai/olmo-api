550f826e6d3f1d7263fc63bd0339d048fa40d72a
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-07 10:54:22 -0600

    i think this is a better pattern since we get validation on stop and files

--- src/message/v4MessageBlueprint.py
+++ src/message/v4MessageBlueprint.py
@@ -4,9 +4,7 @@ from flask import Blueprint, Response, jsonify, request
 from flask_pydantic_api.api_wrapper import pydantic_api
 from flask_pydantic_api.utils import UploadedFile
 from pydantic import (
-    ConfigDict,
     Field,
-    SkipValidation,
     field_serializer,
     model_validator,
 )
@@ -21,11 +19,10 @@ class CreateMessageRequest(APIInterface):
     parent: Optional[str] = Field(default=None)
     max_tokens: int
     temperature: float
-    n: Optional[int] = Field(default=1, ge=1, le=50, multiple_of=1)
+    n: Optional[int] = Field(default=1, ge=1, le=1, multiple_of=1)
     top_p: Optional[float] = Field(default=1.0, ge=0.01, le=1.0, multiple_of=0.01)
     logprobs: Optional[int] = Field(default=None, ge=0, le=10, multiple_of=1)
     # Mapping for this is handled in the controller
-    stop: SkipValidation[Optional[list[str]]] = Field(default=None)
     content: str = Field(min_length=1)
     role: Role
     original: Optional[str] = Field(default=None)
@@ -34,11 +31,6 @@ class CreateMessageRequest(APIInterface):
     template: Optional[str] = Field(default=None)
     model_id: str
     host: str
-
-    # Mapping for this is handled in the controller
-    files: SkipValidation[Optional[list[UploadedFile]]] = Field(default=None)
-
-    model_config = ConfigDict(arbitrary_types_allowed=True)
 
     @model_validator(mode="after")
     def check_original_and_parent_are_different(self) -> Self:
@@ -53,6 +45,11 @@ class CreateMessageRequest(APIInterface):
             raise ValueError("Assistant messages must have a parent")
 
         return self
+
+
+class CreateMessageRequestWithLists(CreateMessageRequest):
+    stop: Optional[list[str]] = Field(default=None)
+    files: Optional[list[UploadedFile]] = Field(default=None)
 
     # TODO: Remove this when we have real output
     @field_serializer("files")
@@ -69,15 +66,17 @@ def create_v4_message_blueprint(dbc: db.Client) -> Blueprint:
     @v4_message_blueprint.post("/stream")
     @pydantic_api(name="Stream a prompt response", tags=["v4", "message"])
     def create_message(create_message_request: CreateMessageRequest) -> Response:
-        # HACK: flask-pydantic-api has poor support for lists in form data, this gets around that for files
-        create_message_request.files = cast(
-            list[UploadedFile], request.files.getlist("files")
+        files = cast(list[UploadedFile], request.files.getlist("files"))
+
+        stop_words = request.form.getlist("stop")
+
+        # HACK: flask-pydantic-api has poor support for lists in form data
+        # Making a separate class that handles lists works for now
+        create_message_request_with_lists = CreateMessageRequestWithLists(
+            **create_message_request.model_dump(), files=files, stop=stop_words
         )
 
-        # HACK: flask-pydantic-api has poor support for lists in form data, this gets around that for stop words
-        create_message_request.stop = request.form.getlist("stop")
-
-        return jsonify(create_message_request.model_dump())
+        return jsonify(create_message_request_with_lists.model_dump())
 
     return v4_message_blueprint
 

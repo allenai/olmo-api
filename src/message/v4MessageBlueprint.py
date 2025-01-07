836a286f39b2f3b77dd3b1cdf0600216cdb4a2a5
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-06 18:13:16 -0600

    able to upload lists and files now

--- src/message/v4MessageBlueprint.py
+++ src/message/v4MessageBlueprint.py
@@ -1,9 +1,15 @@
-import logging
-from typing import Optional
+from typing import Optional, Self, cast
 
 from flask import Blueprint, Response, jsonify, request
-from pydantic import ConfigDict, Field
-from werkzeug.datastructures import FileStorage
+from flask_pydantic_api.api_wrapper import pydantic_api
+from flask_pydantic_api.utils import UploadedFile
+from pydantic import (
+    ConfigDict,
+    Field,
+    SkipValidation,
+    field_serializer,
+    model_validator,
+)
 
 from src import db
 from src.api_interface import APIInterface
@@ -11,38 +17,67 @@ from src.dao.message import Role
 
 
 class CreateMessageRequest(APIInterface):
+    # TODO: Validate that the parent role is different from this role and that it exists
     parent: Optional[str] = Field(default=None)
     max_tokens: int
     temperature: float
-    n: int
-    top_p: float
-    logprobs: Optional[int] = Field(default=None)
-    stop: Optional[list[str]] = Field(default=None)
+    n: Optional[int] = Field(default=1, ge=1, le=50, multiple_of=1)
+    top_p: Optional[float] = Field(default=1.0, ge=0.01, le=1.0, multiple_of=0.01)
+    logprobs: Optional[int] = Field(default=None, ge=0, le=10, multiple_of=1)
+    # Mapping for this is handled in the controller
+    stop: SkipValidation[Optional[list[str]]] = Field(default=None)
     content: str = Field(min_length=1)
     role: Role
-    original: str
+    original: Optional[str] = Field(default=None)
     private: bool
     root: Optional[str] = Field(default=None)
-    template: str
+    template: Optional[str] = Field(default=None)
     model_id: str
     host: str
-    image: Optional[FileStorage] = Field(default=None)
+
+    # Mapping for this is handled in the controller
+    files: SkipValidation[Optional[list[UploadedFile]]] = Field(default=None)
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    @model_validator(mode="after")
+    def check_original_and_parent_are_different(self) -> Self:
+        if self.original is not None and self.parent == self.original:
+            raise ValueError("The original message cannot also be the parent")
+
+        return self
+
+    @model_validator(mode="after")
+    def check_assistant_message_has_a_parent(self) -> Self:
+        if self.role is Role.Assistant and self.parent is None:
+            raise ValueError("Assistant messages must have a parent")
+
+        return self
+
+    # TODO: Remove this when we have real output
+    @field_serializer("files")
+    def serialize_files(self, files: Optional[list[UploadedFile]]):
+        if files is not None:
+            return [file.filename for file in files]
+        else:
+            return None
 
 
 def create_v4_message_blueprint(dbc: db.Client) -> Blueprint:
     v4_message_blueprint = Blueprint("message", __name__)
 
-    # @v4_message_blueprint.post("/stream")
-    def create_message() -> Response:
-        logger = logging.getLogger()
-        logger.info(str(request.form.to_dict()))
-        mapped_request = CreateMessageRequest(**request.form, **request.files)
+    @v4_message_blueprint.post("/stream")
+    @pydantic_api(name="Stream a prompt response", tags=["v4", "message"])
+    def create_message(create_message_request: CreateMessageRequest) -> Response:
+        # HACK: flask-pydantic-api has poor support for lists in form data, this gets around that for files
+        create_message_request.files = cast(
+            list[UploadedFile], request.files.getlist("files")
+        )
 
-        return jsonify(mapped_request)
+        # HACK: flask-pydantic-api has poor support for lists in form data, this gets around that for stop words
+        create_message_request.stop = request.form.getlist("stop")
 
-    v4_message_blueprint.post("/stream")(create_message)
+        return jsonify(create_message_request.model_dump())
 
     return v4_message_blueprint
 

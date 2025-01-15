337f2adc79987d74bea284ebbdc1a42ba5739b53
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-15 09:48:21 -0600

    Validate the model ID we get in the attribution endpoint (#196)
    Addresses https://github.com/allenai/playground-issues-repo/issues/108
    
    I kept seeing 500s in our logs from our model -> index mapping not quite
    working properly. This fixes that and sets up pydantic validation on the
    `attribution` endpoint.
    
    I also changed how we output Pydantic validation errors. Before we were
    only getting a string representation of the error. With the change we're
    getting the list of validation errors in the JSON as well. This will
    likely be useful for finishing up the issue this helps address.
    
    A bad request with the new validation error mapping:
    ```
    {
      "error": {
        "code": 400,
        "message": "1 validation error for GetAttributionRequest\nmodel_id\n  Value error, OLMoE-1B-7B-0924-Instruc must be one of ['olmo-7b-base', 'olmo-7b-chat', 'OLMo-peteish-dpo-preview', 'OLMoE-1B-7B-0924-Instruct', 'OLMo-2-1124-13B-Instruct'] [type=value_error, input_value='OLMoE-1B-7B-0924-Instruc', input_type=str]\n    For further information visit https://errors.pydantic.dev/2.10/v/value_error",
        "validation_errors": [
          {
            "input": "OLMoE-1B-7B-0924-Instruc",
            "loc": [
              "model_id"
            ],
            "msg": "Value error, OLMoE-1B-7B-0924-Instruc must be one of ['olmo-7b-base', 'olmo-7b-chat', 'OLMo-peteish-dpo-preview', 'OLMoE-1B-7B-0924-Instruct', 'OLMo-2-1124-13B-Instruct']",
            "type": "value_error",
            "url": "https://errors.pydantic.dev/2.10/v/value_error"
          }
        ]
      }
    }
    ```
    
    
    What it looked like before:
    ```
    {
      "error": {
        "code": 400,
        "message": "1 validation error for GetAttributionRequest\nmodel_id\n  Value error, OLMoE-1B-7B-0924-Instruc must be one of ['olmo-7b-base', 'olmo-7b-chat', 'OLMo-peteish-dpo-preview', 'OLMoE-1B-7B-0924-Instruct', 'OLMo-2-1124-13B-Instruct'] [type=value_error, input_value='OLMoE-1B-7B-0924-Instruc', input_type=str]\n    For further information visit https://errors.pydantic.dev/2.10/v/value_error"
      }
    }
    ```

--- src/error.py
+++ src/error.py
@@ -1,11 +1,16 @@
-from flask import jsonify, request, render_template, make_response
-from flask.typing import ResponseReturnValue
-from werkzeug.exceptions import HTTPException
-from werkzeug import http
-from typing import Optional
 from logging import getLogger
+from typing import Optional
 
-def make_error_response(code: Optional[int] = None, message: Optional[str] = None) -> ResponseReturnValue:
+from flask import jsonify, make_response, render_template, request
+from flask.typing import ResponseReturnValue
+from pydantic import ValidationError
+from werkzeug import http
+from werkzeug.exceptions import HTTPException
+
+
+def make_error_response(
+    code: Optional[int] = None, message: Optional[str | dict] = None
+) -> ResponseReturnValue:
     if code is None:
         code = 500
     if message is None:
@@ -13,17 +18,30 @@ def make_error_response(code: Optional[int] = None, message: Optional[str] = Non
     accept = http.parse_accept_header(request.headers.get("Accept", "application/json"))
     match accept.best:
         case "text/html":
-            return make_response(render_template("error.jinja", title=f"HTTP {code}", message=message), code)
+            return make_response(
+                render_template("error.jinja", title=f"HTTP {code}", message=message),
+                code,
+            )
         case _:
-            return make_response(jsonify({ "error": { "code": code, "message": message }}), code)
+            if isinstance(message, str):
+                return make_response(
+                    jsonify({"error": {"code": code, "message": message}}), code
+                )
+            else:
+                return make_response(jsonify({"error": {"code": code, **message}}))
+
 
 def handle(e: Exception) -> ResponseReturnValue:
     getLogger(__name__).exception(e)
     if isinstance(e, HTTPException):
         return make_error_response(e.code, e.description)
+    if isinstance(e, ValidationError):
+        return make_error_response(
+            400,
+            {"message": str(e), "validation_errors": e.errors(include_context=False)},
+        )
     if isinstance(e, ValueError):
         return make_error_response(400, str(e))
     if isinstance(e, NotImplementedError):
         return make_error_response(400, str(e))
     return make_error_response(500, "Internal Server Error")
-

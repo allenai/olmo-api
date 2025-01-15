b9ed542bb37c6e0399b84c49c41baeee03c89310
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-15 09:48:21 -0600

    Validate the model ID we get in the attribution endpoint (#196)
    Addresses https://github.com/allenai/playground-issues-repo/issues/108
    
    I kept seeing 500s in our logs from our model -> index mapping not quite
    working properly. This fixes that and sets up pydantic validation on the
    `attribution` endpoint.
    
    I also changed how we output Pydantic validation errors. Before we were
    only getting a string representation of the error. With the change we're
    getting the list of validation errors in the JSON as well. This will
    likely be useful for finishing up the issue this helps address.
    
    A bad request with the new validation error mapping:
    ```
    {
      "error": {
        "code": 400,
        "message": "1 validation error for GetAttributionRequest\nmodel_id\n  Value error, OLMoE-1B-7B-0924-Instruc must be one of ['olmo-7b-base', 'olmo-7b-chat', 'OLMo-peteish-dpo-preview', 'OLMoE-1B-7B-0924-Instruct', 'OLMo-2-1124-13B-Instruct'] [type=value_error, input_value='OLMoE-1B-7B-0924-Instruc', input_type=str]\n    For further information visit https://errors.pydantic.dev/2.10/v/value_error",
        "validation_errors": [
          {
            "input": "OLMoE-1B-7B-0924-Instruc",
            "loc": [
              "model_id"
            ],
            "msg": "Value error, OLMoE-1B-7B-0924-Instruc must be one of ['olmo-7b-base', 'olmo-7b-chat', 'OLMo-peteish-dpo-preview', 'OLMoE-1B-7B-0924-Instruct', 'OLMo-2-1124-13B-Instruct']",
            "type": "value_error",
            "url": "https://errors.pydantic.dev/2.10/v/value_error"
          }
        ]
      }
    }
    ```
    
    
    What it looked like before:
    ```
    {
      "error": {
        "code": 400,
        "message": "1 validation error for GetAttributionRequest\nmodel_id\n  Value error, OLMoE-1B-7B-0924-Instruc must be one of ['olmo-7b-base', 'olmo-7b-chat', 'OLMo-peteish-dpo-preview', 'OLMoE-1B-7B-0924-Instruct', 'OLMo-2-1124-13B-Instruct'] [type=value_error, input_value='OLMoE-1B-7B-0924-Instruc', input_type=str]\n    For further information visit https://errors.pydantic.dev/2.10/v/value_error"
      }
    }
    ```

--- src/attribution/attribution_blueprint.py
+++ src/attribution/attribution_blueprint.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, Response, jsonify, request
-from werkzeug import exceptions
+from flask import Blueprint, Response, jsonify
+from flask_pydantic_api.api_wrapper import pydantic_api
 
-from src.attribution.attribution_service import get_attribution
+from src.attribution.attribution_service import GetAttributionRequest, get_attribution
 from src.config import cfg
 
 from .infini_gram_api_client import Client
@@ -10,16 +10,16 @@ attribution_blueprint = Blueprint(name="attribution", import_name=__name__)
 
 
 @attribution_blueprint.post(rule="")
-def get_attribution_for_model_response() -> Response:
-    if request.content_type != "application/json":
-        raise exceptions.UnsupportedMediaType
-
-    if request.json is None:
-        raise exceptions.BadRequest("missing JSON body")
-
+@pydantic_api(
+    name="Get CorpusLink spans and documents from a prompt", tags=["CorpusLink"]
+)
+def get_attribution_for_model_response(
+    corpuslink_request: GetAttributionRequest,
+) -> Response:
     infini_gram_client = Client(base_url=cfg.infini_gram.api_url)
 
     attribution_response = get_attribution(
+        request=corpuslink_request,
         infini_gram_client=infini_gram_client,
     )
 

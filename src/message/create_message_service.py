64c9ad20c50188439be1b3a791fd670304374a7a
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-29 18:03:18 -0600

    cleanup

--- src/message/create_message_service.py
+++ src/message/create_message_service.py
@@ -387,46 +387,9 @@ def stream_new_message(
 
             # We handle the first chunk differently since we want to timeout if it takes longer than 5 seconds
             first_chunk = results.get(5.0)
-            chunk_logprobs = (
-                first_chunk.logprobs if first_chunk.logprobs is not None else []
-            )
-            mapped_logprobs = [
-                [
-                    message.TokenLogProbs(
-                        token_id=lp.token_id, text=lp.text, logprob=lp.logprob
-                    )
-                    for lp in lp_list
-                ]
-                for lp_list in chunk_logprobs
-            ]
-
             yield map_chunk(first_chunk)
 
             for chunk in message_generator:
-                finish_reason = chunk.finish_reason
-
-                chunk_logprobs = chunk.logprobs if chunk.logprobs is not None else []
-                mapped_logprobs = [
-                    [
-                        message.TokenLogProbs(
-                            token_id=lp.token_id, text=lp.text, logprob=lp.logprob
-                        )
-                        for lp in lp_list
-                    ]
-                    for lp_list in chunk_logprobs
-                ]
-
-                new_chunk = message.MessageChunk(
-                    message=reply.id,
-                    content=chunk.content,
-                    logprobs=mapped_logprobs,
-                )
-                chunks.append(new_chunk)
-
-                input_token_count = chunk.input_token_count
-                output_token_count = chunk.output_token_count
-                chunk_count += 1
-                first_ns = time_ns() if first_ns == 0 else first_ns
                 yield map_chunk(chunk)
 
         except grpc.RpcError as e:

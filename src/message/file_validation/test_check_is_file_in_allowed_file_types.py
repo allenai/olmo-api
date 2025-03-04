87e4d18d1562608c1ec6c581cfaeadc4d0ca3057
Taylor Blanton <matthewtblanton@gmail.com>
2025-03-04 14:45:41 -0600

    add a pdf to test with

--- src/message/file_validation/test_check_is_file_in_allowed_file_types.py
+++ src/message/file_validation/test_check_is_file_in_allowed_file_types.py
@@ -19,3 +19,21 @@ def test_returns_false_when_passed_a_png():
 
         application_result = check_is_file_in_allowed_file_types(f, ["application/*"])
         assert application_result is False
+
+
+def test_returns_true_when_passed_a_pdf():
+    with open(os.path.join(os.path.dirname(__file__), "test-small-pdf.pdf"), "rb") as f:
+        png_result = check_is_file_in_allowed_file_types(f, allowed_file_types=["application/pdf"])
+        assert png_result is True
+
+        star_result = check_is_file_in_allowed_file_types(f, allowed_file_types=["application/*"])
+        assert star_result is True
+
+
+def test_returns_false_when_passed_a_pdf():
+    with open(os.path.join(os.path.dirname(__file__), "test-small-pdf.pdf"), "rb") as f:
+        mp4_result = check_is_file_in_allowed_file_types(f, ["video/mp4"])
+        assert mp4_result is False
+
+        application_result = check_is_file_in_allowed_file_types(f, ["image/*"])
+        assert application_result is False

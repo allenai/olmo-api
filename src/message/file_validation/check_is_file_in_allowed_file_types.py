cd9cfe95c71ae2666fcca82ff0453c6019561aca
Taylor Blanton <matthewtblanton@gmail.com>
2025-03-04 17:30:02 -0600

    skip checking file types if allowed_file_types is empty

--- src/message/file_validation/check_is_file_in_allowed_file_types.py
+++ src/message/file_validation/check_is_file_in_allowed_file_types.py
@@ -11,6 +11,10 @@ def match_mime_type(file_mime_type: str, matching_mime_type: str):
 
 
 def check_is_file_in_allowed_file_types(file_stream: typing.IO[bytes], allowed_file_types: Sequence[str]):
+    # This is partially here to allow us to keep testing without using real files!
+    if len(allowed_file_types) == 0:
+        return True
+
     potential_mime_types = puremagic.magic_stream(file_stream)
 
     return any(

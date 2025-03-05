36834e0c7cbac43aa1538e2399078aee9a352f56
Taylor Blanton <matthewtblanton@gmail.com>
2025-03-04 18:24:32 -0600

    extra wasn't working

--- src/config/get_models.py
+++ src/config/get_models.py
@@ -14,9 +14,7 @@ def get_model_by_host_and_id(host: str, id: str) -> Model:
     model = next((model for model in cfg.models if model.host == host and model.id == id), None)
 
     if model is None:
-        logging.getLogger().error(
-            "Couldn't find model/host combination %(model)s/%(host)s", extra={"model": id, "host": host}
-        )
+        logging.getLogger().error("Couldn't find model/host combination %s/%s", id, host)
 
         error_message = f"Invalid model/host combination {id}/{host}"
         raise exceptions.BadRequest(error_message)

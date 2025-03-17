bfc27225320723e1b0837e441f78a4c07ca584e1
Jiacheng Liu <liujch1998@gmail.com>
2025-03-17 22:54:07 +0000

    Add correct index for 32B and 1B-7B

--- src/attribution/infini_gram_api_client/models/available_infini_gram_index_id.py
+++ src/attribution/infini_gram_api_client/models/available_infini_gram_index_id.py
@@ -2,7 +2,9 @@ from enum import StrEnum
 
 
 class AvailableInfiniGramIndexId(StrEnum):
+    OLMOE_0125_1B_7B = "olmoe-0125-1b-7b"
     OLMO_2_1124_13B = "olmo-2-1124-13b"
+    OLMO_2_0325_32B = "olmo-2-0325-32b"
     PILEVAL_LLAMA = "pileval-llama"
 
     def __str__(self) -> str:

cf7d05776351b5b8fb165e64023d51cc217fb8f7
Taylor Blanton <matthewtblanton@gmail.com>
2025-04-15 14:01:19 -0500

    separate the connection pools

--- src/db/connection_pool.py
+++ src/db/connection_pool.py
@@ -3,7 +3,7 @@ import os
 from functools import lru_cache
 
 from psycopg_pool import ConnectionPool
-from sqlalchemy import URL, create_pool_from_url, make_url
+from sqlalchemy import URL, make_url
 
 
 def make_psycopg3_url(conninfo) -> URL:
@@ -11,17 +11,13 @@ def make_psycopg3_url(conninfo) -> URL:
 
 
 @lru_cache
-def create_connection_pool(conninfo: str, min_size: int, max_size: int):
+def create_connection_pool(conninfo: str, min_size: int, max_size: int) -> ConnectionPool:
     logging.getLogger().debug(
         "Creating connection pool for worker pid %s with min_size %s, max_size %s",
         os.getpid(),
         min_size,
         max_size,
     )
-
-    url = make_psycopg3_url(conninfo)
-
-    return create_pool_from_url(url)
 
     return ConnectionPool(
         conninfo=conninfo,
--- src/db/init_sqlalchemy.py
+++ src/db/init_sqlalchemy.py
@@ -7,6 +7,4 @@ def make_db_engine(config: Database) -> Engine:
     url = make_url(config.conninfo)
     url_with_correct_driver = url.set(drivername="postgresql+psycopg")
 
-    return create_engine(
-        url_with_correct_driver,
-    )
+    return create_engine(url_with_correct_driver, pool_size=1, max_overflow=config.max_size)

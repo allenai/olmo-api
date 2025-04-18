400051360d74406ac3e10e1342e2ec51eb92aff3
yensung <george129.chen@gmail.com>
2025-04-18 14:13:20 +0800

    Include source information

---
+++ src/config/InfiniGramSource.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, ModelWrapValidatorHandler, model_validator
+
+from typing import Union
+from typing_extensions import Self
+
+
+class InfiniGramSource(BaseModel):
+    name: str
+    usage: str
+    display_name: Union[str, None]
+    url: Union[str, None]
+    secondary_name: Union[str, None]
+
+    @model_validator(mode='wrap')
+    @classmethod
+    def generate_initial_values(cls, data: dict, handler: ModelWrapValidatorHandler[Self]):
+        
+        initial_values = {
+            "name": data.get('name'),
+            "usage": data.get('usage'),
+            "display_name": data.get("display_name") or data.get("name"),
+            "url": data.get('url') or f"https://huggingface.co/datasets/allenai/{data.get('name')}",
+            "secondary_name": data.get('secondary_name')
+        }
+
+        validated_values = handler(initial_values)
+            
+        return validated_values
+
+
+def map_infinigram_sources(infinigram_sources) -> dict[str, InfiniGramSource]:
+    source_dict = {}
+    for item in infinigram_sources or []:
+        validated_source = InfiniGramSource.model_validate(item)
+        source_dict[validated_source.name] = validated_source
+
+    return source_dict

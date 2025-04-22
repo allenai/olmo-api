40814350727adbab13c49c680570643e97a77db2
yensung <george129.chen@gmail.com>
2025-04-22 10:01:49 +0800

    feedback

--- src/config/InfiniGramSource.py
+++ src/config/InfiniGramSource.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel, ModelWrapValidatorHandler, model_validator
+from typing import Self, Union
 
-from typing import Union
-from typing_extensions import Self
+from pydantic import BaseModel, ModelWrapValidatorHandler, model_validator
 
 
 class InfiniGramSource(BaseModel):
@@ -11,24 +10,23 @@ class InfiniGramSource(BaseModel):
     url: Union[str, None]
     secondary_name: Union[str, None]
 
-    @model_validator(mode='wrap')
+    @model_validator(mode="wrap")
     @classmethod
     def generate_initial_values(cls, data: dict, handler: ModelWrapValidatorHandler[Self]):
-        
         initial_values = {
-            "name": data.get('name'),
-            "usage": data.get('usage'),
+            "name": data.get("name"),
+            "usage": data.get("usage"),
             "display_name": data.get("display_name") or data.get("name"),
-            "url": data.get('url') or f"https://huggingface.co/datasets/allenai/{data.get('name')}",
-            "secondary_name": data.get('secondary_name')
+            "url": data.get("url") or f"https://huggingface.co/datasets/allenai/{data.get('name')}",
+            "secondary_name": data.get("secondary_name"),
         }
 
         validated_values = handler(initial_values)
-            
+
         return validated_values
 
 
-def map_infinigram_sources(infinigram_sources) -> dict[str, InfiniGramSource]:
+def map_infinigram_sources(infinigram_sources: list[dict]) -> dict[str, InfiniGramSource]:
     source_dict = {}
     for item in infinigram_sources or []:
         validated_source = InfiniGramSource.model_validate(item)

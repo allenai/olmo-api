ec925258ee35e652bcca22ea095bcbc32f5b70af
Taylor Blanton <matthewtblanton@gmail.com>
2025-04-23 17:06:17 -0500

    Handle unique constraint for creating models (#284)
    This PR addresses the unique constraints error not being handled
    properly (as called out by @yensung, ty!)
    
    It also messes with the error handling more, turns out not all
    httpexceptions have JSON as their body.
    
    Example duplicate response:
    ```json
    {
      "error": {
        "code": 409,
        "message": "tulu3-405b26 already exists"
      }
    }
    ```

--- src/model_config/create_model_config_service.py
+++ src/model_config/create_model_config_service.py
@@ -1,7 +1,10 @@
 from datetime import UTC
 
+from psycopg.errors import UniqueViolation
 from pydantic import AwareDatetime, Field
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, sessionmaker
+from werkzeug import exceptions
 
 from src.api_interface import APIInterface
 from src.config.ModelConfig import ModelHost, ModelType
@@ -27,28 +30,36 @@ class CreateModelConfigRequest(APIInterface):
 
 def create_model_config(
     request: CreateModelConfigRequest, session_maker: sessionmaker[Session]
-):
+) -> ResponseModel:
     with session_maker.begin() as session:
-        new_model = ModelConfig(
-            id=request.id,
-            name=request.name,
-            host=request.host,
-            description=request.description,
-            model_type=request.model_type,
-            model_id_on_host=request.model_id_on_host,
-            internal=request.internal,
-            prompt_type=request.prompt_type,
-            default_system_prompt=request.default_system_prompt,
-            family_id=request.family_id,
-            family_name=request.family_name,
-            available_time=request.available_time.astimezone(UTC)
-            if request.available_time is not None
-            else None,
-            deprecation_time=request.deprecation_time.astimezone(UTC)
-            if request.deprecation_time is not None
-            else None,
-        )
-        session.add(new_model)
-        session.flush()  # This populates the auto-generated things on the new_model
+        try:
+            new_model = ModelConfig(
+                id=request.id,
+                name=request.name,
+                host=request.host,
+                description=request.description,
+                model_type=request.model_type,
+                model_id_on_host=request.model_id_on_host,
+                internal=request.internal,
+                prompt_type=request.prompt_type,
+                default_system_prompt=request.default_system_prompt,
+                family_id=request.family_id,
+                family_name=request.family_name,
+                available_time=request.available_time.astimezone(UTC)
+                if request.available_time is not None
+                else None,
+                deprecation_time=request.deprecation_time.astimezone(UTC)
+                if request.deprecation_time is not None
+                else None,
+            )
+            session.add(new_model)
+            session.flush()  # This populates the auto-generated things on the new_model
 
-        return ResponseModel.model_validate(new_model)
+            return ResponseModel.model_validate(new_model)
+
+        except IntegrityError as e:
+            if isinstance(e.orig, UniqueViolation):
+                conflict_message = f"{request.id} already exists"
+                raise exceptions.Conflict(conflict_message) from e
+
+            raise

4ba9e946e237c2e92b88b4b9d77c26dd5616227f
yensung <george129.chen@gmail.com>
2025-04-24 18:39:39 +0800

    Add get models endpoint

--- src/model_config/model_config_blueprint.py
+++ src/model_config/model_config_blueprint.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, Response, jsonify
+from authlib.integrations.flask_oauth2 import current_token
+from flask import Blueprint, Response, jsonify, request
 from flask_pydantic_api.api_wrapper import pydantic_api
 from sqlalchemy import select
 from sqlalchemy.orm import Session, selectin_polymorphic, sessionmaker
 from werkzeug import exceptions
 
+from src.auth.resource_protectors import required_auth_protector
 from src.dao.engine_models.model_config import ModelConfig, MultiModalModelConfig
 from src.model_config.create_model_config_service import (
     CreateModelConfigRequest,
@@ -14,23 +16,31 @@ from src.model_config.delete_model_config_service import DeleteModelConfigReques
 
 
 def create_model_config_blueprint(session_maker: sessionmaker[Session]) -> Blueprint:
-    from src.auth.resource_protectors import required_auth_protector
-
     model_config_blueprint = Blueprint(name="model_config", import_name=__name__)
 
     @model_config_blueprint.get("/")
-    @pydantic_api(
-        name="Get available models and their configuration", tags=["v4", "models"]
-    )
+    @pydantic_api(name="Get available models and their configuration", tags=["v4", "models"])
     def get_model_configs() -> Response:
+        is_admin = request.args.get("admin")
+
         with session_maker.begin() as session:
-            polymorphic_loader_opt = selectin_polymorphic(
-                ModelConfig, [ModelConfig, MultiModalModelConfig]
-            )
+            polymorphic_loader_opt = selectin_polymorphic(ModelConfig, [ModelConfig, MultiModalModelConfig])
             stmt = select(ModelConfig).options(polymorphic_loader_opt)
             rows = session.scalars(stmt).all()
 
-            return jsonify(rows)
+            # return everything for admin roles
+            if is_admin == "true" or (
+                "permissions" in current_token and "read:internal-models" in current_token.permissions
+            ):
+                models = list(rows)
+                models.sort(key=lambda m: m.order)
+                return jsonify(models)
+
+            # otherwise, filter out internal models
+            models = list(filter(lambda m: not m.internal, rows))
+            models.sort(key=lambda m: m.order)
+
+            return jsonify(models)
 
     @model_config_blueprint.post("/")  # type: ignore
     @required_auth_protector("write:model-config")
@@ -46,13 +56,12 @@ def create_model_config_blueprint(session_maker: sessionmaker[Session]) -> Bluep
 
     @model_config_blueprint.delete("/")  # type: ignore
     @required_auth_protector("write:model-config")
-    @pydantic_api(
-        name="Delete a model", 
-        tags=["v4", "models", "model configuration"])
+    @pydantic_api(name="Delete a model", tags=["v4", "models", "model configuration"])
     def delete_model(request: DeleteModelConfigRequest):
         try:
             delete_model_config(request, session_maker)
             return
         except ValueError as e:
-            raise  exceptions.NotFound
+            raise exceptions.NotFound
+
     return model_config_blueprint

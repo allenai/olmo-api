e6ffb614a24a16ce0c128a7a4980ce3f2018357f
Taylor Blanton <matthewtblanton@gmail.com>
2025-04-24 10:57:56 -0500

    have model delete take in a path param (#288)
    Our previous version didn't take in a path param, now it does!

--- src/model_config/delete_model_config_service.py
+++ src/model_config/delete_model_config_service.py
@@ -1,16 +1,11 @@
-
-from src.api_interface import APIInterface
-
 from sqlalchemy.orm import Session, sessionmaker
+
 from src.dao.engine_models.model_config import ModelConfig
 
-class DeleteModelConfigRequest(APIInterface):
-    id: str
 
-def delete_model_config(request: DeleteModelConfigRequest, session_maker: sessionmaker[Session]):
+def delete_model_config(model_id: str, session_maker: sessionmaker[Session]):
     with session_maker.begin() as session:
-        delete_model = session.query(ModelConfig).filter_by(id=request.id).first()
+        delete_model = session.query(ModelConfig).filter_by(id=model_id).first()
         if not delete_model:
             raise ValueError("Model config not found!")
         session.delete(delete_model)
-

9cfaa81dc3e2742464649981a787e0ab62e2a0dc
Taylor Blanton <matthewtblanton@gmail.com>
2025-04-25 18:54:43 -0500

    fix polymorphic updating

--- src/model_config/update_model_config_service.py
+++ src/model_config/update_model_config_service.py
@@ -1,12 +1,11 @@
-from typing import Literal
+from typing import Literal, cast
 
 from pydantic import AwareDatetime, Field, RootModel
-from sqlalchemy import update
 from sqlalchemy.orm import Session, sessionmaker
 
 from src.api_interface import APIInterface
 from src.config.ModelConfig import FileRequiredToPromptOption, ModelHost, ModelType
-from src.dao.engine_models.model_config import ModelConfig, PromptType
+from src.dao.engine_models.model_config import MultiModalModelConfig, PromptType
 from src.model_config.model_config_utils import get_model_config_class
 from src.model_config.response_model import ResponseModel
 
@@ -52,15 +51,40 @@ def update_model_config(
 ) -> ResponseModel | None:
     with session_maker.begin() as session:
         RequestClass = get_model_config_class(request.root)
-        updated_model = session.scalar(
-            update(RequestClass)
-            .where(ModelConfig.id == model_id)
-            .values(request.model_dump())
-            .returning(ModelConfig),
-        )
+        model_to_update = session.get(RequestClass, model_id)
 
-        return (
-            ResponseModel.model_validate(updated_model)
-            if updated_model is not None
-            else None
-        )
+        if model_to_update is None:
+            return None
+
+        model_to_update.name = request.root.name
+        model_to_update.host = request.root.host
+        model_to_update.description = request.root.description
+        model_to_update.model_type = request.root.model_type
+        model_to_update.model_id_on_host = request.root.model_id_on_host
+        model_to_update.internal = request.root.internal
+        model_to_update.default_system_prompt = request.root.default_system_prompt
+        model_to_update.family_id = request.root.family_id
+        model_to_update.family_name = request.root.family_name
+        model_to_update.available_time = request.root.available_time
+        model_to_update.deprecation_time = request.root.deprecation_time
+
+        if isinstance(model_to_update, MultiModalModelConfig):
+            multi_modal_request = cast(UpdateMultiModalModelConfigRequest, request.root)
+
+            model_to_update.accepted_file_types = (
+                multi_modal_request.accepted_file_types
+            )
+            model_to_update.max_files_per_message = (
+                multi_modal_request.max_files_per_message
+            )
+            model_to_update.require_file_to_prompt = (
+                multi_modal_request.require_file_to_prompt
+            )
+            model_to_update.max_total_file_size = (
+                multi_modal_request.max_total_file_size
+            )
+            model_to_update.allow_files_in_followups = (
+                multi_modal_request.allow_files_in_followups
+            )
+
+        return ResponseModel.model_validate(model_to_update)

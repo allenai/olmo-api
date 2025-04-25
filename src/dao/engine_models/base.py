00909f8a9dec2954831d7f2ac61c1c385c6f47d3
Taylor Blanton <matthewtblanton@gmail.com>
2025-04-25 16:56:08 -0500

    WIP

--- src/dao/engine_models/base.py
+++ src/dao/engine_models/base.py
@@ -1,8 +1,43 @@
-from datetime import datetime
+import datetime
+from typing import Optional
 
-from sqlalchemy import TIMESTAMP
+from sqlalchemy import DateTime, Dialect, TypeDecorator
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
 
+# Taken from https://github.com/litestar-org/advanced-alchemy/blob/27f6f7e48aff36f4ee80b50f7e8268790b1acb4d/advanced_alchemy/types/datetime.py
+class DateTimeUTC(TypeDecorator[datetime.datetime]):
+    """Timezone Aware DateTime.
+
+    Ensure UTC is stored in the database and that TZ aware dates are returned for all dialects.
+    """
+
+    impl = DateTime(timezone=True)
+    cache_ok = True
+
+    @property
+    def python_type(self) -> type[datetime.datetime]:
+        return datetime.datetime
+
+    def process_bind_param(
+        self, value: Optional[datetime.datetime], dialect: Dialect
+    ) -> Optional[datetime.datetime]:
+        if value is None:
+            return value
+        if not value.tzinfo:
+            msg = "tzinfo is required"
+            raise TypeError(msg)
+        return value.astimezone(datetime.timezone.utc)
+
+    def process_result_value(
+        self, value: Optional[datetime.datetime], dialect: Dialect
+    ) -> Optional[datetime.datetime]:
+        if value is None:
+            return value
+        if value.tzinfo is None:
+            return value.replace(tzinfo=datetime.timezone.utc)
+        return value
+
+
 class Base(MappedAsDataclass, DeclarativeBase):
-    type_annotation_map = {datetime: TIMESTAMP(timezone=True)}
+    type_annotation_map = {datetime.datetime: DateTimeUTC}
--- src/model_config/create_model_config_service.py
+++ src/model_config/create_model_config_service.py
@@ -1,18 +1,23 @@
-from datetime import UTC
+from typing import Annotated, Literal
 
 from psycopg.errors import UniqueViolation
-from pydantic import AwareDatetime, Field
+from pydantic import AwareDatetime, Field, RootModel
+from sqlalchemy import insert
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, sessionmaker
 from werkzeug import exceptions
 
 from src.api_interface import APIInterface
-from src.config.ModelConfig import ModelHost, ModelType
+from src.config.ModelConfig import (
+    FileRequiredToPromptOption,
+    ModelHost,
+    ModelType,
+)
 from src.dao.engine_models.model_config import ModelConfig, PromptType
 from src.model_config.response_model import ResponseModel
 
 
-class CreateModelConfigRequest(APIInterface):
+class BaseCreateModelConfigRequest(APIInterface):
     id: str
     name: str
     host: ModelHost
@@ -20,7 +25,6 @@ class CreateModelConfigRequest(APIInterface):
     model_type: ModelType
     model_id_on_host: str
     internal: bool = Field(default=True)
-    prompt_type: PromptType
     default_system_prompt: str | None = Field(default=None)
     family_id: str | None = Field(default=None)
     family_name: str | None = Field(default=None)
@@ -28,32 +32,56 @@ class CreateModelConfigRequest(APIInterface):
     deprecation_time: AwareDatetime | None = Field(default=None)
 
 
+class CreateTextOnlyModelConfigRequest(BaseCreateModelConfigRequest):
+    prompt_type: Literal[PromptType.TEXT_ONLY]
+
+
+class CreateMultiModalModelConfigRequest(BaseCreateModelConfigRequest):
+    prompt_type: Literal[PromptType.MULTI_MODAL]
+    accepted_file_types: list[str]
+    max_files_per_message: int | None = Field(default=None)
+    require_file_to_prompt: FileRequiredToPromptOption | None = Field(default=None)
+    max_total_file_size: int | None = Field(default=None)
+    allow_files_in_followups: bool | None = Field(default=None)
+
+
+CreateModelConfigRequest = RootModel[
+    Annotated[
+        CreateTextOnlyModelConfigRequest | CreateMultiModalModelConfigRequest,
+        Field(discriminator="prompt_type"),
+    ]
+]
+
+
 def create_model_config(
     request: CreateModelConfigRequest, session_maker: sessionmaker[Session]
 ) -> ResponseModel:
     with session_maker.begin() as session:
         try:
-            new_model = ModelConfig(
-                id=request.id,
-                name=request.name,
-                host=request.host,
-                description=request.description,
-                model_type=request.model_type,
-                model_id_on_host=request.model_id_on_host,
-                internal=request.internal,
-                prompt_type=request.prompt_type,
-                default_system_prompt=request.default_system_prompt,
-                family_id=request.family_id,
-                family_name=request.family_name,
-                available_time=request.available_time.astimezone(UTC)
-                if request.available_time is not None
-                else None,
-                deprecation_time=request.deprecation_time.astimezone(UTC)
-                if request.deprecation_time is not None
-                else None,
+            new_model = session.scalar(
+                insert(ModelConfig).values(request.model_dump()).returning(ModelConfig)
             )
-            session.add(new_model)
-            session.flush()  # This populates the auto-generated things on the new_model
+            # new_model = ModelConfig(
+            #     id=request.id,
+            #     name=request.name,
+            #     host=request.host,
+            #     description=request.description,
+            #     model_type=request.model_type,
+            #     model_id_on_host=request.model_id_on_host,
+            #     internal=request.internal,
+            #     prompt_type=request.prompt_type,
+            #     default_system_prompt=request.default_system_prompt,
+            #     family_id=request.family_id,
+            #     family_name=request.family_name,
+            #     available_time=request.available_time.astimezone(UTC)
+            #     if request.available_time is not None
+            #     else None,
+            #     deprecation_time=request.deprecation_time.astimezone(UTC)
+            #     if request.deprecation_time is not None
+            #     else None,
+            # )
+            # session.add(new_model)
+            # session.flush()  # This populates the auto-generated things on the new_model
 
             return ResponseModel.model_validate(new_model)
 

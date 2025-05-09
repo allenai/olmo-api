b7dd9e1939e8332637ef07eac4ea1614aa7deb0c
Taylor Blanton <matthewtblanton@gmail.com>
2025-05-09 14:20:20 -0500

    share psycopg's connection pool with sqlalchemy

--- src/db/init_sqlalchemy.py
+++ src/db/init_sqlalchemy.py
@@ -1,3 +1,4 @@
+from psycopg_pool import ConnectionPool
 from sqlalchemy import URL, Engine, create_engine, make_url
 
 from src.config.Config import Database
@@ -7,7 +8,7 @@ def make_psycopg3_url(conninfo) -> URL:
     return make_url(conninfo).set(drivername="postgresql+psycopg")
 
 
-def make_db_engine(config: Database) -> Engine:
+def make_db_engine(config: Database, pool: ConnectionPool) -> Engine:
     url = make_psycopg3_url(config.conninfo)
 
-    return create_engine(url, pool_size=1, max_overflow=config.max_size)
+    return create_engine(url, creator=pool.getconn)

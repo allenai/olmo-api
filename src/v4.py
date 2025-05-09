34ecb1ca4e030827000cafbdf03f4e7a1b8759ec
Taylor Blanton <matthewtblanton@gmail.com>
2025-05-09 14:01:11 -0500

    make v4/models endpoints available without feature flag

--- src/v4.py
+++ src/v4.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 from sqlalchemy.orm import Session, sessionmaker
 
 from src import db
-from src.config.get_config import get_config
 from src.message.GoogleCloudStorage import GoogleCloudStorage
 from src.message.v4MessageBlueprint import create_v4_message_blueprint
 from src.model_config.model_config_blueprint import create_model_config_blueprint
@@ -10,7 +9,6 @@ from src.model_config.model_config_blueprint import create_model_config_blueprin
 
 def create_v4_blueprint(dbc: db.Client, storage_client: GoogleCloudStorage, session_maker: sessionmaker[Session]):
     v4_blueprint = Blueprint(name="v4", import_name=__name__)
-    config = get_config()
 
     v4_blueprint.register_blueprint(
         blueprint=create_v4_message_blueprint(dbc, storage_client=storage_client, session_maker=session_maker),
@@ -18,9 +16,6 @@ def create_v4_blueprint(dbc: db.Client, storage_client: GoogleCloudStorage, sess
         name="message",
     )
 
-    if config.feature_flags.enable_dynamic_model_config:
-        v4_blueprint.register_blueprint(
-            create_model_config_blueprint(session_maker), url_prefix="/models", name="models"
-        )
+    v4_blueprint.register_blueprint(create_model_config_blueprint(session_maker), url_prefix="/models", name="models")
 
     return v4_blueprint

cf82f6bed66d330b56ec662e0a71fbb835f14aa0
Taylor Blanton <matthewtblanton@gmail.com>
2025-05-13 17:37:26 -0500

    fix test_model.py

--- src/config/test_Model.py
+++ src/config/test_Model.py
@@ -6,7 +6,7 @@ from src.config.Model import map_model_from_config
 from src.config.ModelConfig import ModelConfig, ModelHost, ModelType
 
 
-@time_machine.travel(datetime(2025, 1, 1))
+@time_machine.travel(datetime(2025, 1, 1, tzinfo=UTC))
 def test_is_not_visible_when_not_available_yet() -> None:
     test_model_config: ModelConfig = {
         "id": "foo",
@@ -20,6 +20,7 @@ def test_is_not_visible_when_not_available_yet() -> None:
         "family_id": None,
         "family_name": None,
         "deprecation_time": None,
+        "internal": False,
     }
 
     model = map_model_from_config(test_model_config)
@@ -28,7 +29,7 @@ def test_is_not_visible_when_not_available_yet() -> None:
     assert model.is_visible is False
 
 
-@time_machine.travel(datetime(2025, 1, 1))
+@time_machine.travel(datetime(2025, 1, 1, tzinfo=UTC))
 def test_is_not_visible_when_past_deprecated_time() -> None:
     test_model_config: ModelConfig = {
         "id": "foo",
@@ -42,6 +43,7 @@ def test_is_not_visible_when_past_deprecated_time() -> None:
         "family_id": None,
         "family_name": None,
         "deprecation_time": datetime(2024, 1, 1).astimezone(UTC).isoformat(),
+        "internal": False,
     }
 
     model = map_model_from_config(test_model_config)
@@ -50,7 +52,7 @@ def test_is_not_visible_when_past_deprecated_time() -> None:
     assert model.is_visible is False
 
 
-@time_machine.travel(datetime(2025, 1, 1))
+@time_machine.travel(datetime(2025, 1, 1, tzinfo=UTC))
 def test_is_not_visible_when_past_deprecated_time_and_available_time() -> None:
     test_model_config: ModelConfig = {
         "id": "foo",
@@ -64,6 +66,7 @@ def test_is_not_visible_when_past_deprecated_time_and_available_time() -> None:
         "family_id": None,
         "family_name": None,
         "deprecation_time": datetime(2024, 1, 1).astimezone(UTC).isoformat(),
+        "internal": False,
     }
 
     model = map_model_from_config(test_model_config)
@@ -72,7 +75,7 @@ def test_is_not_visible_when_past_deprecated_time_and_available_time() -> None:
     assert model.is_visible is False
 
 
-@time_machine.travel(datetime(2025, 1, 1))
+@time_machine.travel(datetime(2025, 1, 1, tzinfo=UTC))
 def test_is_visible_when_past_available_time() -> None:
     test_model_config: ModelConfig = {
         "id": "foo",
@@ -86,6 +89,7 @@ def test_is_visible_when_past_available_time() -> None:
         "family_id": None,
         "family_name": None,
         "deprecation_time": None,
+        "internal": False,
     }
 
     model = map_model_from_config(test_model_config)
@@ -94,7 +98,7 @@ def test_is_visible_when_past_available_time() -> None:
     assert model.is_visible is True
 
 
-@time_machine.travel(datetime(2025, 1, 1))
+@time_machine.travel(datetime(2025, 1, 1, tzinfo=UTC))
 def test_is_visible_when_no_available_time() -> None:
     test_model_config: ModelConfig = {
         "id": "foo",
@@ -108,6 +112,7 @@ def test_is_visible_when_no_available_time() -> None:
         "family_id": None,
         "family_name": None,
         "deprecation_time": None,
+        "internal": False,
     }
 
     model = map_model_from_config(test_model_config)
@@ -116,7 +121,7 @@ def test_is_visible_when_no_available_time() -> None:
     assert model.is_visible is True
 
 
-@time_machine.travel(datetime(2025, 1, 1))
+@time_machine.travel(datetime(2025, 1, 1, tzinfo=UTC))
 def test_is_visible_when_deprecation_time_is_in_the_future() -> None:
     test_model_config: ModelConfig = {
         "id": "foo",
@@ -130,6 +135,7 @@ def test_is_visible_when_deprecation_time_is_in_the_future() -> None:
         "family_id": None,
         "family_name": None,
         "deprecation_time": datetime(2026, 1, 1).astimezone(UTC).isoformat(),
+        "internal": False,
     }
 
     model = map_model_from_config(test_model_config)

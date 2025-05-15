b1195906e9bd2e3ef8178ab006f17dbaff41ec4b
Taylor Blanton <matthewtblanton@gmail.com>
2025-05-15 10:59:02 -0500

    format BeakerQueuesEngine

--- src/inference/BeakerQueuesEngine.py
+++ src/inference/BeakerQueuesEngine.py
@@ -2,8 +2,8 @@ from collections.abc import Generator, Sequence
 from dataclasses import asdict
 
 from beaker import Beaker
-from google.protobuf import json_format
 from beaker.config import Config as BeakerConfig
+from google.protobuf import json_format
 
 from src.config.get_config import get_config
 from src.inference.InferenceEngine import (
@@ -15,6 +15,7 @@ from src.inference.InferenceEngine import (
 
 EXPIRES_IN = 60 * 60 * 24 * 30  # 30 days
 
+
 class BeakerQueuesEngine(InferenceEngine):
     beaker_client: Beaker
 
@@ -25,19 +26,18 @@ class BeakerQueuesEngine(InferenceEngine):
     def create_streamed_message(
         self, model: str, messages: Sequence[InferenceEngineMessage], inference_options: InferenceOptions
     ) -> Generator[InferenceEngineChunk, None, None]:
-
         q = self.beaker_client.queue.get(model)
         input = {
             "model": q.id,
             "messages": [asdict(message) for message in messages],
             "stream": True,
-            "opts": asdict(inference_options)
+            "opts": asdict(inference_options),
         }
         for m in self.beaker_client.queue.create_entry(q, input=input, expires_in_sec=EXPIRES_IN):
             if m.HasField("pending_entry"):
                 pass
             if m.HasField("result"):
                 result = json_format.MessageToDict(m.result)
-                yield InferenceEngineChunk(content=result['choices'][0]['delta']['content'], model=model)
+                yield InferenceEngineChunk(content=result["choices"][0]["delta"]["content"], model=model)
             if m.HasField("finalized_entry"):
                 pass

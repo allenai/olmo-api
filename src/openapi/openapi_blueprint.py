77ec09cfe2969cced40eae58f33b0ec4b020475f
Taylor Blanton <matthewtblanton@gmail.com>
2025-05-30 15:06:13 -0500

    get the FlatMessage type into OpenAPI

--- src/openapi/openapi_blueprint.py
+++ src/openapi/openapi_blueprint.py
@@ -4,12 +4,25 @@ from flask import Blueprint, render_template
 from flask_pydantic_api.openapi import get_openapi_schema
 from pydantic.json_schema import GenerateJsonSchema
 
+from src.message.v5_message_blueprint import FlatMessage
+
 openapi_blueprint = Blueprint("openapi", __name__, template_folder="templates")
 
 
 @openapi_blueprint.get("/openapi.json")
 def get_openapi_spec() -> dict[str, Any]:
-    schema = get_openapi_schema(schema_generator=GenerateJsonSchema)
+    schema = get_openapi_schema(
+        schema_generator=GenerateJsonSchema,
+    )
+
+    # HACK: FlatMessage is a response from the v5/message/stream endpoint. OpenAPI doesn't support typing streamed responses so we need to add the types here
+    flat_message_schema = FlatMessage.model_json_schema(ref_template="#/components/schemas/{model}")
+    flat_message_schema_defs = flat_message_schema.pop("$defs")
+    schema["components"]["schemas"] = {
+        **schema["components"]["schemas"],
+        **flat_message_schema_defs,
+        FlatMessage.__name__: flat_message_schema,
+    }
     return schema
 
 

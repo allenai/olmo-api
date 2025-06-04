65badf1cf8b855eb8fc70a9d4ca48eed63e83e7c
Taylor Blanton <matthewtblanton@gmail.com>
2025-06-04 09:06:25 -0500

    Make new Thread endpoints that do message mapping for the client (#312)
    Closes https://github.com/allenai/playground-issues-repo/issues/406
    
    This PR is meant to make the UI's job easier. Since we're doing some
    refactoring around message streaming anyway this seemed like a good
    opportunity to move this functionality. It's basically a straight port
    of what we had in v3/v4 messages but with `/threads` instead of
    `/messages` or `/message`
    
    There's some deeper changes we could make so that our logic doesn't
    involve nesting these, but mapping at the controller level works for
    now!

--- src/api_interface.py
+++ src/api_interface.py
@@ -3,4 +3,6 @@ from pydantic.alias_generators import to_camel
 
 
 class APIInterface(BaseModel):
-    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True, from_attributes=True)
+    model_config = ConfigDict(
+        alias_generator=to_camel, populate_by_name=True, from_attributes=True, serialize_by_alias=True
+    )
---
+++ src/thread/threads_blueprint.py
@@ -0,0 +1,86 @@
+from collections.abc import Generator
+from typing import Any, cast
+
+from flask import Blueprint, Response, jsonify, request, stream_with_context
+from flask.typing import ResponseReturnValue
+from flask_pydantic_api.api_wrapper import pydantic_api
+from flask_pydantic_api.utils import UploadedFile
+from pydantic import ValidationError
+from sqlalchemy.orm import Session, sessionmaker
+
+from src import db
+from src.api_interface import APIInterface
+from src.dao import message
+from src.error import handle_validation_error
+from src.message.create_message_request import (
+    CreateMessageRequest,
+    CreateMessageRequestWithLists,
+)
+from src.message.create_message_service import (
+    create_message_v4,
+    format_message,
+)
+from src.message.GoogleCloudStorage import GoogleCloudStorage
+from src.message.message_service import get_message
+from src.thread.get_threads_service import GetThreadsRequest, GetThreadsResponse, get_threads
+from src.thread.thread_models import Thread
+
+
+def format_messages(
+    stream_generator: Generator[message.Message | message.MessageChunk | message.MessageStreamError, Any, None],
+) -> Generator[str, Any, None]:
+    for stream_message in stream_generator:
+        match stream_message:
+            case message.Message():
+                flat_messages = Thread.from_message(stream_message)
+
+                yield format_message(flat_messages)
+            case APIInterface():
+                yield format_message(stream_message)
+
+
+def create_threads_blueprint(
+    dbc: db.Client, storage_client: GoogleCloudStorage, session_maker: sessionmaker[Session]
+) -> Blueprint:
+    threads_blueprint = Blueprint("messages", __name__)
+
+    @threads_blueprint.get("/")
+    @pydantic_api(name="Get messages", tags=["v5", "messages"])
+    def list_threads(request: GetThreadsRequest) -> GetThreadsResponse:
+        return get_threads(dbc, request)
+
+    @threads_blueprint.get("/<string:id>")
+    @pydantic_api(name="Get message", tags=["v5", "messages"])
+    def get_single_thread(id: str) -> Thread:
+        return Thread.from_message(get_message(id, dbc))
+
+    @threads_blueprint.post("/")
+    @pydantic_api(name="Stream a prompt response", tags=["v5", "messages"])
+    def create_message(
+        create_message_request: CreateMessageRequest,
+    ) -> ResponseReturnValue:
+        request_files = request.files.getlist("files")
+        # Defaulting to an empty list can cause problems with Modal
+        # This isn't happening from the UI but it is happening through e2e tests, so better safe than sorry!
+        files = cast(list[UploadedFile], request_files) if len(request_files) > 0 else None
+
+        stop_words = request.form.getlist("stop")
+
+        try:
+            # HACK: flask-pydantic-api has poor support for lists in form data
+            # Making a separate class that handles lists works for now
+            create_message_request_with_lists = CreateMessageRequestWithLists(
+                **create_message_request.model_dump(), files=files, stop=stop_words
+            )
+
+            stream_response = create_message_v4(
+                create_message_request_with_lists, dbc, storage_client=storage_client, session_maker=session_maker
+            )
+            if isinstance(stream_response, Generator):
+                return Response(stream_with_context(format_messages(stream_response)), mimetype="application/jsonl")
+            return jsonify(stream_response)
+
+        except ValidationError as e:
+            return handle_validation_error(e)
+
+    return threads_blueprint

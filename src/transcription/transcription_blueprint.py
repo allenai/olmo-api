c8a0c6da76f586e49f87af239f8a1ef86b0f524e
Taylor Blanton <matthewtblanton@gmail.com>
2025-06-12 12:56:33 -0500

    formatting, small cleanup

--- src/transcription/transcription_blueprint.py
+++ src/transcription/transcription_blueprint.py
@@ -1,15 +1,8 @@
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint
 from flask_pydantic_api.api_wrapper import pydantic_api
 
-from pydantic import ValidationError
+from src.transcription.transcription_service import GetTranscriptionRequest, GetTranscriptionResponse, get_transcription
 
-from src.config.get_config import cfg
-from src.error import handle_validation_error
-from src.transcription.transcription_service import (
-    GetTranscriptionRequest,
-    GetTranscriptionResponse,
-    get_transcription
-)
 
 def create_transcription_blueprint() -> Blueprint:
     transcription_blueprint = Blueprint("transcription", __name__)
@@ -17,9 +10,6 @@ def create_transcription_blueprint() -> Blueprint:
     @transcription_blueprint.post("/")
     @pydantic_api(name="Transcribe audio", tags=["v4", "transcribe"])
     def transcribe(request: GetTranscriptionRequest) -> GetTranscriptionResponse:
-        try:
-            return get_transcription(request)
-        except ValidationError as e:
-            return handle_validation_error(e)
+        return get_transcription(request)
 
     return transcription_blueprint

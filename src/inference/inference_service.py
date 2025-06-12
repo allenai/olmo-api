53bcaff1f42bfd99af08bb456e40990f7805b4cf
Taylor Blanton <matthewtblanton@gmail.com>
2025-06-12 16:38:40 -0500

    add a separate olmoasr engine

--- src/inference/inference_service.py
+++ src/inference/inference_service.py
@@ -2,22 +2,27 @@ from collections.abc import Sequence
 
 from src.config import get_config
 from src.config.Model import Model, MultiModalModel
+from src.constants import OLMO_ASR_MODEL_ID
 from src.dao.engine_models.model_config import ModelHost
 from src.inference.BeakerQueuesEngine import BeakerQueuesEngine
 from src.inference.InferDEngine import InferDEngine
 from src.inference.InferenceEngine import InferenceEngine
 from src.inference.ModalEngine import ModalEngine
+from src.inference.olmo_asr_engine import OlmoAsrModalEngine
 
 
 def get_available_models() -> Sequence[Model | MultiModalModel]:
     return get_config.cfg.models
 
 
-def get_engine(host: ModelHost) -> InferenceEngine:
+def get_engine(host: ModelHost, model: str) -> InferenceEngine:
     match host:
         case ModelHost.InferD:
             return InferDEngine()
         case ModelHost.BeakerQueues:
             return BeakerQueuesEngine()
         case ModelHost.Modal | _:
+            if model == OLMO_ASR_MODEL_ID:
+                # HACK: The OLMoASR model has some special handling. We'll want to correct that in the future
+                return OlmoAsrModalEngine()
             return ModalEngine()

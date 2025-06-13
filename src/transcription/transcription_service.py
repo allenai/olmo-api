08b662d9986a0dc361c14c18181396d9a1183370
Taylor Blanton <matthewtblanton@gmail.com>
2025-06-13 18:30:23 -0500

    remove commented code

--- src/transcription/transcription_service.py
+++ src/transcription/transcription_service.py
@@ -21,8 +21,8 @@ class GetTranscriptionResponse(APIInterface):
 
 
 def get_transcription(request: GetTranscriptionRequest, session_maker: sessionmaker):
-    # audio_bytes = request.audio.read()
     segment = AudioSegment.from_file_using_temporary_files(request.audio)
+
     # .export can return a path with different options but returns IO when we call it without a filename
     converted_audio_file = cast(IO, segment.export(format="wav"))
 
@@ -30,6 +30,7 @@ def get_transcription(request: GetTranscriptionRequest, session_maker: sessionma
 
     model = get_model_by_host_and_id(host="modal", id=OLMO_ASR_MODEL_ID, session_maker=session_maker)
     messages = [InferenceEngineMessage(role=Role.User, content="", files=[converted_audio_file.read()])]
+
     response = next(
         olmo_asr_engine.create_streamed_message(
             model=model.model_id_on_host, messages=messages, inference_options=InferenceOptions()

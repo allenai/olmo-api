5d9fe67ecafd2ae6e8651c690202a8d391bddd67
Taylor Blanton <matthewtblanton@gmail.com>
2025-06-13 14:00:31 -0500

    add .modal.toml and use it instead of making our own clients

--- src/inference/olmo_asr_engine.py
+++ src/inference/olmo_asr_engine.py
@@ -3,7 +3,6 @@ from collections.abc import Generator, Sequence
 import modal
 from werkzeug.exceptions import BadRequest
 
-from src.config.get_config import get_config
 from src.inference.InferenceEngine import (
     InferenceEngine,
     InferenceEngineChunk,
@@ -13,19 +12,13 @@ from src.inference.InferenceEngine import (
 
 
 class OlmoAsrModalEngine(InferenceEngine):
-    client: modal.Client
-
-    def __init__(self) -> None:
-        cfg = get_config()
-        self.client = modal.Client.from_credentials(cfg.modal.token, cfg.modal.token_secret)
-
     def create_streamed_message(
         self,
         model: str,
         messages: Sequence[InferenceEngineMessage],
         inference_options: InferenceOptions,  # noqa: ARG002
     ) -> Generator[InferenceEngineChunk, None, None]:
-        modal_class = modal.Cls.from_name(model, "Model").hydrate(client=self.client)()
+        modal_class = modal.Cls.from_name(model, "Model")
 
         files = next(message.files for message in messages)
 
@@ -40,6 +33,6 @@ class OlmoAsrModalEngine(InferenceEngine):
             no_saved_file_message = "Tried to transcribe a message that had a previously uploaded file"
             raise BadRequest(no_saved_file_message)
 
-        transcription_result = modal_class.transcribe.remote(audio=audio.read())
+        transcription_result = modal_class().transcribe.remote(audio=audio.read())
 
         yield InferenceEngineChunk(content=transcription_result.get("text"), model=model)

2432790915611c0a1cee23c27764a3fef0c60d09
Taylor Blanton <matthewtblanton@gmail.com>
2025-07-09 12:59:14 -0500

    cleanup

--- src/auth/auth0_bearer_token_validator.py
+++ src/auth/auth0_bearer_token_validator.py
@@ -1,11 +1,8 @@
 import json
-import logging
 from urllib.request import urlopen
 
 from authlib.jose.rfc7517.jwk import JsonWebKey
 from authlib.oauth2.rfc7523 import JWTBearerTokenValidator
-
-logger = logging.getLogger(__name__)
 
 OLD_AUTH0_ISSUER = "https://allenai-public.us.auth0.com/"
 

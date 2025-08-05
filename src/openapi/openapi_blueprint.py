c661d8f15f84b2b36c0d551aebe587e1e445f90d
Taylor Blanton <matthewtblanton@gmail.com>
2025-08-05 18:42:25 -0500

    add new chunks to openapi schema

--- src/openapi/openapi_blueprint.py
+++ src/openapi/openapi_blueprint.py
@@ -2,16 +2,31 @@ from typing import Any
 
 from flask import Blueprint, render_template
 from flask_pydantic_api.openapi import get_openapi_schema
-from pydantic.json_schema import GenerateJsonSchema
+from pydantic import BaseModel
+from pydantic.json_schema import GenerateJsonSchema, JsonSchemaMode, models_json_schema
+
+from src.message.message_chunk import BaseChunk
 
 openapi_blueprint = Blueprint("openapi", __name__, template_folder="templates")
+
+REF_TEMPLATE = "#/components/schemas/{model}"
 
 
 @openapi_blueprint.get("/openapi.json")
 def get_openapi_spec() -> dict[str, Any]:
-    return get_openapi_schema(
+    schema = get_openapi_schema(
         schema_generator=GenerateJsonSchema,
     )
+
+    # HACK: These are responses from the message streaming endpoint. OpenAPI doesn't support typing streamed responses so we need to add the types here
+    chunks: list[tuple[type[BaseModel], JsonSchemaMode]] = [
+        (subclass, "validation") for subclass in BaseChunk.__subclasses__()
+    ]
+    _, top_level_schema = models_json_schema(chunks, ref_template=REF_TEMPLATE)
+
+    schema["components"]["schemas"].update(top_level_schema.get("$defs"))
+
+    return schema
 
 
 @openapi_blueprint.get("/docs")

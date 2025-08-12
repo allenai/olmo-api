ee95985ac4f87c999d8c8f16f2f7e0d204412df7
Taylor Blanton <matthewtblanton@gmail.com>
2025-08-12 18:21:59 -0500

    move repository, make the label filter work for getting a message

---
+++ src/dao/message/message_repository.py
@@ -0,0 +1,102 @@
+import abc
+from collections.abc import Sequence
+from typing import Any
+
+from sqlalchemy import delete, func, or_, select
+from sqlalchemy.orm import Session, joinedload
+
+from src import obj
+from src.api_interface import APIInterface
+from src.dao import paged
+from src.dao.engine_models.label import Label
+from src.dao.engine_models.message import Message
+from src.dao.message.message_models import ThreadList
+from src.dao.paged import Opts
+
+
+class ToolCall(APIInterface):
+    tool_name: str
+    args: str | dict[str, Any] | None = None
+    tool_call_id: str
+
+
+class BaseMessageRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, message: Message) -> Message:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, message_id: obj.ID, user_id: str) -> Sequence[Message] | None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def update(self, message: Message) -> Message:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def delete(self, message_id: obj.ID) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_threads_for_user(self, user_id: str, sort_opts: Opts) -> ThreadList:
+        raise NotImplementedError
+
+
+class MessageRepository(BaseMessageRepository):
+    session: Session
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, message: Message) -> Message:
+        self.session.add(message)
+        self.session.flush()
+
+        return self.session.get_one(Message, message.id)
+
+    def get(self, message_id: obj.ID, user_id: str) -> Sequence[Message] | None:
+        query = (
+            select(Message)
+            .where(Message.root == message_id)
+            .where(or_(Message.expiration_time == None, Message.expiration_time > func.now()))  # noqa: E711
+            .options(joinedload(Message.labels.and_(Label.deleted == None, Label.creator == user_id)))  # noqa: E711
+            .order_by(Message.created.asc())
+        )
+
+        return self.session.scalars(query).unique().all()
+
+    def update(self, message: Message) -> Message:
+        message_to_update = self.session.get_one(Message, message.id)
+
+        for var, value in vars(message).items():
+            setattr(message_to_update, var, value) if value else None
+
+        self.session.flush()
+        return message_to_update
+
+    def delete(self, message_id: obj.ID) -> None:
+        self.session.execute(delete(Message).where(Message.id == message_id))
+
+    def get_threads_for_user(self, user_id: str, sort_opts: Opts) -> ThreadList:
+        thread_conditions = [
+            Message.creator == user_id,
+            Message.final == True,  # noqa: E712
+            Message.parent == None,  # noqa: E711
+            or_(Message.expiration_time == None, Message.expiration_time > func.now()),  # noqa: E711
+        ]
+
+        total = self.session.query(Message.id).where(*thread_conditions).count()
+
+        select_messages = select(Message).where(*thread_conditions).order_by(Message.created.desc())
+
+        if sort_opts.limit is not None:
+            select_messages = select_messages.limit(sort_opts.limit)
+
+        if sort_opts.offset is not None:
+            select_messages = select_messages.offset(sort_opts.offset)
+
+        threads = self.session.scalars(select_messages).unique().all()
+
+        return ThreadList(
+            threads=threads, meta=paged.ListMeta(total=total, offset=sort_opts.offset, limit=sort_opts.limit)
+        )

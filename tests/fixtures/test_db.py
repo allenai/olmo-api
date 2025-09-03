03e53f48ab6425a0dabfa185542d4be9c5be694c
Caleb Ouellette <calebo@allenai.org>
2025-09-03 16:33:40 -0700

    wip

--- tests/fixtures/test_db.py
+++ tests/fixtures/test_db.py
@@ -1,8 +1,11 @@
 import logging
 from pathlib import Path
 
+import pytest
 from psycopg import Connection
 from pytest_postgresql import factories
+
+from src.config.Config import Config
 
 LOGGER = logging.getLogger(__name__)
 
@@ -20,8 +23,22 @@ postgresql = factories.postgresql(
 )
 
 
+@pytest.fixture(scope="function")
+def app():
+    app = create_app()
+    yield app
+
+
 def test_example_postgres(postgresql: Connection):
     """Check main postgresql fixture."""
+
+    connection = f"postgresql+psycopg2://{postgresql.info.user}:@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
+
+    dbc = db.Client.from_config(cfg.db)
+    # db_engine = make_db_engine(cfg.db, pool=dbc.pool, sql_alchemy=cfg.sql_alchemy)
+    # session_maker = sessionmaker(db_engine, expire_on_commit=False, autoflush=True)
+    # flask_scoped_session(session_maker, app=app)
+
     cur = postgresql.cursor()
     result = cur.execute("""
     select * from message limit 10;

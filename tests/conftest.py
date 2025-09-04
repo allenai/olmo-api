331ffd9d10cb0e6388d136d83aff1dac039e032a
Caleb Ouellette <calebo@allenai.org>
2025-09-04 16:48:40 -0700

    small refactor

---
+++ tests/conftest.py
@@ -0,0 +1,1 @@
+from tests.fixtures.db_fixtures import dbc, sql_alchemy
---
+++ tests/test_simple.py
@@ -0,0 +1,6 @@
+from src import db
+
+
+def test_db(dbc: db.Client):
+    result = dbc.message.get("123")
+    assert result is not None

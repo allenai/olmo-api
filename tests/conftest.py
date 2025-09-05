ac8c3f26a86120529200478655c898d3ba6decf7
Caleb Ouellette <calebo@allenai.org>
2025-09-04 17:59:10 -0700

    working but might change the order.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,10 @@ from sqlalchemy.orm import sessionmaker, Session as SessionMaker
 from src import db
 from src.config import get_config
 from src.db.init_sqlalchemy import make_db_engine
+from pytest_postgresql.executor import PostgreSQLExecutor
+from pytest_postgresql.janitor import DatabaseJanitor
 
+LOGGER = logging.getLogger(__name__)
 
 postgresql_proc = factories.postgresql_proc(
     load=[
@@ -24,31 +27,52 @@ postgresql = factories.postgresql(
 )
 
 
-@pytest.fixture
-def dbc(postgresql: Connection):
+@pytest.fixture(scope="session")
+def database_setup(postgresql_proc: PostgreSQLExecutor):
+    logging.getLogger().error("Setting up DB")
+    with DatabaseJanitor(
+        user=postgresql_proc.user,
+        host=postgresql_proc.host,
+        port=postgresql_proc.port,
+        dbname=postgresql_proc.dbname,
+        version=postgresql_proc.version,
+        template_dbname=postgresql_proc.template_dbname,
+    ):
+        yield
+
+
+@pytest.fixture(scope="session")
+def sql_alchemy_session_maker(database_setup, postgresql_proc: PostgreSQLExecutor):
+    logging.getLogger().error("Starting up SQL Alchemy session maker")
     cfg = get_config.Config.load("./test.config.json")
 
     cfg.db.conninfo = (
-        f"postgresql://{postgresql.info.user}:@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
+        f"postgresql://{postgresql_proc.user}:@{postgresql_proc.host}:{postgresql_proc.port}/{postgresql_proc.dbname}"
     )
-
-    return db.Client.from_config(cfg.db)
-
-
-@pytest.fixture
-def sql_alchemy_session_maker(dbc: db.Client, postgresql: Connection):
-    cfg = get_config.Config.load("./test.config.json")
-
-    cfg.db.conninfo = (
-        f"postgresql://{postgresql.info.user}:@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
-    )
+    dbc = db.Client.from_config(cfg.db)
 
     db_engine = make_db_engine(cfg.db, pool=dbc.pool, sql_alchemy=cfg.sql_alchemy)
     session_maker = sessionmaker(db_engine, expire_on_commit=False, autoflush=True)
-    return session_maker
+
+    yield session_maker
+    db_engine.dispose()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
+def dbc(database_setup, postgresql_proc: PostgreSQLExecutor):
+    cfg = get_config.Config.load("./test.config.json")
+
+    cfg.db.conninfo = (
+        f"postgresql://{postgresql_proc.user}:@{postgresql_proc.host}:{postgresql_proc.port}/{postgresql_proc.dbname}"
+    )
+
+    dbc = db.Client.from_config(cfg.db)
+    yield dbc
+    dbc.close()
+
+
+@pytest.fixture
 def sql_alchemy(sql_alchemy_session_maker: sessionmaker[SessionMaker]):
+    logging.getLogger().error("Starting up SQL Alchemy session")
     with sql_alchemy_session_maker() as session:
         yield session
--- tests/create_message_service/test_database.py
+++ tests/create_message_service/test_database.py
@@ -42,3 +42,37 @@ def test_thread_setup(sql_alchemy: Session):
 
     assert len(thread_setup) == 1
     assert thread_setup[0].role == Role.System
+
+
+def test_thread_setup_two(sql_alchemy: Session):
+    message_repo = MessageRepository(sql_alchemy)
+    request = CreateMessageRequestWithFullMessages(
+        captcha_token="",
+        parent_id=None,
+        parent=None,
+        opts=InferenceOpts(),
+        content="Hello world",
+        role=Role.User,
+        model="123",
+        host="123",
+        client="xyz",
+    )
+
+    model = ModelConfig(
+        id="123",
+        host=ModelHost.TestBackend,
+        name="",
+        description="",
+        model_type=ModelType.Chat,
+        model_id_on_host="",
+        internal=True,
+        prompt_type=PromptType.TEXT_ONLY,
+        default_system_prompt="You are an Ai! Go take over the world!",
+    )
+
+    token = Token(client="1234", is_anonymous_user=False, token="hello")
+
+    thread_setup = setup_msg_thread(message_repository=message_repo, model=model, request=request, agent=token)
+
+    assert len(thread_setup) == 1
+    assert thread_setup[0].role == Role.System

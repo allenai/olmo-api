99a425cab80e5881c701145fc279caf93e39cff5
Caleb Ouellette <calebo@allenai.org>
2025-09-08 14:02:08 -0700

    config clean up

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from pytest_postgresql import factories
 from sqlalchemy.orm import sessionmaker
 
 from src.config import get_config
+from src.config.Config import Config
 from src.db import Client
 from src.db.init_sqlalchemy import make_db_engine
 
@@ -23,26 +24,25 @@ postgresql = factories.postgresql(
 
 
 @pytest.fixture
-def dbc(postgresql: Connection):
+def cfg(postgresql: Connection):
     cfg = get_config.Config.load("./test.config.json")
 
+    # dynamically set connection based on pytest_postgresql
     cfg.db.conninfo = (
         f"postgresql://{postgresql.info.user}:@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
     )
+    return cfg
 
+
+@pytest.fixture
+def dbc(cfg: Config):
     dbc = Client.from_config(cfg.db)
     yield dbc
     dbc.close()
 
 
 @pytest.fixture
-def sql_alchemy(postgresql: Connection, dbc: Client):
-    cfg = get_config.Config.load("./test.config.json")
-
-    cfg.db.conninfo = (
-        f"postgresql://{postgresql.info.user}:@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
-    )
-
+def sql_alchemy(dbc: Client, cfg: Config):
     db_engine = make_db_engine(cfg.db, pool=dbc.pool, sql_alchemy=cfg.sql_alchemy)
     session_maker = sessionmaker(db_engine, expire_on_commit=False, autoflush=True)
 

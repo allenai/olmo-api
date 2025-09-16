8f56e98752eadf2e55426528d39eaf4cea66b633
Taylor Blanton <matthewtblanton@gmail.com>
2025-09-16 14:13:14 -0700

    Configurable google text moderation thresholds (#384)
    Closes https://github.com/allenai/playground-issues-repo/issues/593
    
    This PR makes the google text moderation threshold configurable. it also
    bumps the defaults up and improves the logging.

--- src/message/GoogleModerateText.py
+++ src/message/GoogleModerateText.py
@@ -8,7 +8,8 @@ from google.cloud.language_v2 import (
     ModerateTextResponse,
 )
 
-from src.config import get_config
+from src.api_interface import APIInterface
+from src.config.get_config import get_config
 from src.message.SafetyChecker import (
     SafetyChecker,
     SafetyCheckRequest,
@@ -16,58 +17,61 @@ from src.message.SafetyChecker import (
 )
 
 
+class ViolationInfo(APIInterface):
+    category_name: str
+    confidence: float
+    confidence_threshold: float
+
+    severity: float
+    severity_threshold: float
+
+
 class GoogleModerateTextResponse(SafetyCheckResponse):
     result: ModerateTextResponse
-    confidence_threshold = 0.5
-    severity_threshold = 0.5
-    unsafe_violation_categories = [
-        "Toxic",
-        "Derogatory",
-        "Violent",
-        "Sexual",
-        "Insult",
-        "Profanity",
-        "Death, Harm & Tragedy",
-        "Firearms & Weapons",
-        "Public Safety",
-        "War & Conflict",
-        "Dangerous Content",
-    ]
+    confidence_threshold: float
+    severity_threshold: float
+    unsafe_violation_categories: list[str]
 
     def __init__(self, result: ModerateTextResponse):
         self.result = result
 
+        config = get_config()
+        self.confidence_threshold = config.google_moderate_text.default_confidence_threshold
+        self.severity_threshold = config.google_moderate_text.default_severity_threshold
+        self.unsafe_violation_categories = config.google_moderate_text.default_unsafe_violation_categories
+
     def is_safe(self) -> bool:
-        violations = self.get_violation_categories()
+        violations = self.get_violations()
 
         return len(violations) == 0
 
-    def get_violation_categories(self) -> list[str]:
-        violations = []
-
-        for category in self.result.moderation_categories:
-            if (
-                category.name in self.unsafe_violation_categories
-                and category.confidence >= self.confidence_threshold
-                and category.severity >= self.severity_threshold
-            ):
-                violations.append(f"<{category.name}> confidence: {category.confidence}; severity: {category.severity}")
-
-        return violations
+    def get_violations(self) -> list[ViolationInfo]:
+        return [
+            ViolationInfo(
+                category_name=category.name,
+                confidence=category.confidence,
+                confidence_threshold=self.confidence_threshold,
+                severity=category.severity,
+                severity_threshold=self.severity_threshold,
+            )
+            for category in self.result.moderation_categories
+            if category.name in self.unsafe_violation_categories
+            and category.confidence >= self.confidence_threshold
+            and category.severity >= self.severity_threshold
+        ]
 
     def get_scores(self):
-        scores = []
-        for category in self.result.moderation_categories:
-            scores.append({"name": category.name, "confidence": category.confidence, "severity": category.severity})
-
-        return scores
+        return [
+            {"name": category.name, "confidence": category.confidence, "severity": category.severity}
+            for category in self.result.moderation_categories
+        ]
 
 
 class GoogleModerateText(SafetyChecker):
     client: LanguageServiceClient
 
     def __init__(self):
-        self.client = LanguageServiceClient(client_options={"api_key": get_config.cfg.google_cloud_services.api_key})
+        self.client = LanguageServiceClient(client_options={"api_key": get_config().google_cloud_services.api_key})
 
     def check_request(self, req: SafetyCheckRequest) -> SafetyCheckResponse:
         request = ModerateTextRequest(
@@ -81,10 +85,11 @@ class GoogleModerateText(SafetyChecker):
         response = GoogleModerateTextResponse(result)
 
         current_app.logger.info({
+            "event": "safety-check.results",
             "checker": "GoogleModerateText",
             "prompt": req.content,
             "duration_ms": (end_ns - start_ns) / 1_000_000,
-            "violations": response.get_violation_categories(),
+            "violations": [info.model_dump() for info in response.get_violations()],
             "scores": response.get_scores(),
         })
 

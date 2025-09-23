205554c3444577ebbc6595b8ebd6e4c781742c7a
Caleb Ouellette <calebo@allenai.org>
2025-09-23 15:48:35 -0700

    added test for converting to dict.

--- src/dao/engine_models/test_message.py
+++ src/dao/engine_models/test_message.py
@@ -1,31 +1,32 @@
+import dataclasses
+
+import json
 import pytest
 from sqlalchemy.orm import Session
 
-from src.auth.token import Token
 from src.dao.engine_models.message import Message
-from src.dao.engine_models.model_config import ModelConfig, ModelHost, ModelType, PromptType
-from src.dao.message.message_models import InferenceOpts, Role
+from src.dao.engine_models.model_config import ModelType
+from src.dao.message.message_models import Role
 from src.dao.message.message_repository import MessageRepository
-from src.message.create_message_request import (
-    CreateMessageRequestWithFullMessages,
-)
-import dataclasses
-from src.message.create_message_service.database import create_user_message, setup_msg_thread
+
+from src import db, util
 
 
 @pytest.mark.integration
 class TestDatabase:
     def test_convert_database_message_to_json(self, sql_alchemy: Session):
         message_repo = MessageRepository(sql_alchemy)
+        id = "123124"
         message = Message(
+            id=id,
             content="Hello world",
             role=Role.User,
             opts={},
             model_id="123",
             model_host="parent.model_host",
             model_type=ModelType.Chat,
-            root="",
-            parent="parent.id",
+            root=id,
+            parent=None,
             template=None,
             final=True,
             original=None,
@@ -37,4 +38,24 @@ class TestDatabase:
 
         message_repo.add(message)
 
-        dictionary = dataclasses.asdict(message)
+        messageTwo = Message(
+            content="Hello world",
+            role=Role.Assistant,
+            opts={},
+            model_id="123",
+            model_host="parent.model_host",
+            model_type=ModelType.Chat,
+            root=id,
+            parent=id,
+            template=None,
+            final=True,
+            original=None,
+            private=False,
+            harmful=False,
+            expiration_time=None,
+            creator="",
+        )
+
+        dictionary = dataclasses.asdict(messageTwo)
+
+        json_obj = json.dumps(obj=dictionary, cls=util.CustomEncoder) + "\n"

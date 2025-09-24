f324e6b1b2e6dc4617196657358a880310ce7294
Caleb Ouellette <calebo@allenai.org>
2025-09-24 12:02:10 -0700

    force service.name

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace import SpanProcessor, TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, SimpleSpanProcessor
 from opentelemetry.trace import set_tracer_provider
 from sqlalchemy.orm import sessionmaker
@@ -21,9 +21,15 @@ from src.message.GoogleCloudStorage import GoogleCloudStorage
 from src.openapi import openapi_blueprint
 from src.v4 import create_v4_blueprint
 
-resource = Resource.create({
-    "service.name": "olmo-api",
-})
+
+class CustomAttributeSpanProcessor(SpanProcessor):
+    def on_start(self, span, parent_context=None):
+        span.set_attributes({
+            "service.name": "olmo-api",
+        })
+
+    def on_end(self, span):
+        pass
 
 
 def create_app():
@@ -34,7 +40,14 @@ def create_app():
 
     cfg = get_config.Config.load(os.environ.get("FLASK_CONFIG_PATH", get_config.DEFAULT_CONFIG_PATH))
 
+    resource = Resource.create({
+        "service.name": "olmo-api",
+        "service.hello": "world",
+    })
+
     tracer_provider = TracerProvider(resource=resource)
+
+    tracer_provider.add_span_processor(CustomAttributeSpanProcessor())
 
     if cfg.otel.collector_type == "local":
         tracer_provider.add_span_processor(span_processor=SimpleSpanProcessor(OTLPSpanExporter()))

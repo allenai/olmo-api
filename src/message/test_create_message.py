4dd0567bf40c0aa3d6cda0700e0a66e558fac932
Caleb Ouellette <calebo@allenai.org>
2025-10-07 14:11:25 -0700

    Added new line processing.

---
+++ src/message/test_create_message.py
@@ -0,0 +1,13 @@
+from src.dao.message.message import Role
+
+from .create_message_request import CreateMessageRequest
+
+
+def test_message_newline_process():
+    msg = "hello \r\n hello \n hi \r\n hello"
+
+    msg_expected = "hello \n hello \n hi \n hello"
+
+    request = CreateMessageRequest(parent=None, content=msg, role=Role.User, model="test", host="test")
+
+    assert request.content == msg_expected

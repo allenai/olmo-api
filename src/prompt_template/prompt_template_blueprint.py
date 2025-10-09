9fc0fb558d1d3d58b0a9b4851b09d1e5e2a3b91e
Taylor Blanton <matthewtblanton@gmail.com>
2025-10-09 13:55:13 -0500

    make the openapi spec work for prompt templates get

--- src/prompt_template/prompt_template_blueprint.py
+++ src/prompt_template/prompt_template_blueprint.py
@@ -2,15 +2,16 @@ from datetime import datetime
 from typing import Any
 
 from flask import Blueprint
-from groq import APIResponse
+from pydantic import RootModel
 
+from src.api_interface import APIInterface
 from src.dao.engine_models.model_config import ModelType
 from src.dao.engine_models.prompt_template import PromptTemplate
 from src.dao.flask_sqlalchemy_session import current_session
 from src.flask_pydantic_api.api_wrapper import pydantic_api
 
 
-class PromptTemplateResponse(APIResponse):
+class PromptTemplateResponse(APIInterface):
     id: str
     name: str
     content: str
@@ -22,6 +23,10 @@ class PromptTemplateResponse(APIResponse):
     model_type: ModelType
     file_urls: list[str] | None
     tool_definitions: Any  # TODO: Map Tool Definitions to a Pydantic model
+
+
+class PromptTemplateResponseList(RootModel):
+    root: list[PromptTemplateResponse]
 
 
 def get_prompt_templates() -> list[PromptTemplateResponse]:
@@ -38,7 +43,7 @@ def create_prompt_template_blueprint() -> Blueprint:
         tags=["v4", "prompt templates"],
         model_dump_kwargs={"by_alias": True},
     )
-    def get_prompt_templates() -> list[PromptTemplateResponse]:
-        return []
+    def get_prompt_templates() -> PromptTemplateResponseList:
+        return PromptTemplateResponseList([])
 
     return prompt_template_blueprint

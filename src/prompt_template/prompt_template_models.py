1b5d7f65a831cdc1ac2edf8c2130279a4247b046
Taylor Blanton <matthewtblanton@gmail.com>
2025-10-13 12:59:12 -0500

    use the right model for tool defs

--- src/prompt_template/prompt_template_models.py
+++ src/prompt_template/prompt_template_models.py
@@ -5,6 +5,7 @@ from pydantic import RootModel
 
 from src.api_interface import APIInterface
 from src.dao.engine_models.model_config import ModelType
+from src.thread.thread_models import ToolDefinition
 
 
 class PromptTemplateResponse(APIInterface):
@@ -18,7 +19,7 @@ class PromptTemplateResponse(APIInterface):
     opts: dict[str, Any]
     model_type: ModelType
     file_urls: list[str] | None
-    tool_definitions: Any  # TODO: Map Tool Definitions to a Pydantic model
+    tool_definitions: list[ToolDefinition]
 
 
 class PromptTemplateResponseList(RootModel):

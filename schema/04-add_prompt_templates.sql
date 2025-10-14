6449c4b19d0e8febde620ad401d2e0624f2f33a3
Taylor Blanton <matthewtblanton@gmail.com>
2025-10-14 12:37:39 -0500

    add a template with tools

--- schema/04-add_prompt_templates.sql
+++ schema/04-add_prompt_templates.sql
@@ -16,4 +16,46 @@ insert into prompt_template (
     'Chat',
     NULL,
     NULL
-)
+) ON CONFLICT DO NOTHING;
+
+insert into prompt_template (
+    id,
+    name,
+    content,
+    creator,
+    opts,
+    model_type,
+    file_urls,
+    extra_parameters
+) values (
+    'p_tpl_tool_definitions',
+    'prompt template with tool definitions',
+    'Tell me the weather',
+    'taylor',
+    '{}',
+    'Chat',
+    NULL,
+    NULL
+) ON CONFLICT DO NOTHING;
+
+insert into tool_definition (
+    id,
+    name,
+    description,
+    parameters,
+    tool_source
+) values (
+    'td_template_tool',
+    'get_weather',
+    'Get the current weather in a given location',
+    ('{"type": "object","default": null,"required": ["location"],"properties": {"location": {"type": "string","default": {"string_value": "Boston, MA"},"required": [],"properties": null,"description": "The city name of the location for which to get the weather.","propertyOrdering": null}},"description": null,"propertyOrdering": null}'),
+    'USER_DEFINED'
+) ON CONFLICT DO NOTHING;
+
+insert into prompt_template_tool_definition_association (
+    prompt_template_id,
+    tool_definition_id
+) values (
+    'p_tpl_tool_definitions',
+    'td_template_tool'
+) ON CONFLICT DO NOTHING;

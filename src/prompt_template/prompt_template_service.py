6dff64b1ddaaf100d1c61eb714992c6f99eeeda9
Taylor Blanton <matthewtblanton@gmail.com>
2025-10-14 13:11:59 -0500

    split out a service and repository

--- src/prompt_template/prompt_template_service.py
+++ src/prompt_template/prompt_template_service.py
@@ -1,9 +1,8 @@
-from src.dao.engine_models.prompt_template import PromptTemplate
-from src.dao.flask_sqlalchemy_session import current_session
 from src.prompt_template.prompt_template_models import PromptTemplateResponseList
+from src.prompt_template.prompt_template_repository import get_prompt_templates
 
 
 def get_prompt_templates_list() -> PromptTemplateResponseList:
-    prompt_templates = current_session.query(PromptTemplate).where(PromptTemplate.deleted == None).all()  # noqa: E711
+    prompt_templates = get_prompt_templates()
 
     return PromptTemplateResponseList.model_validate(prompt_templates)

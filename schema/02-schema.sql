b553228e5ba00be3720c355eabfbaae7be7884d7
Taylor Blanton <matthewtblanton@gmail.com>
2025-10-27 16:35:36 -0500

    add db migration for agent_id column

--- schema/02-schema.sql
+++ schema/02-schema.sql
@@ -1033,5 +1033,11 @@ ALTER TABLE message ADD COLUMN error_severity TEXT;
 
 UPDATE alembic_version SET version_num='440e4ee34a13' WHERE alembic_version.version_num = '4ecb303b4164';
 
+-- Running upgrade 5da1e4a16ea0 -> 2d0eb25156b5
+
+ALTER TABLE message ADD COLUMN agent_id VARCHAR;
+
+UPDATE alembic_version SET version_num='2d0eb25156b5' WHERE alembic_version.version_num = '440e4ee34a13';
+
 COMMIT;
 

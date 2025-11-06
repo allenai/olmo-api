62eca14cca0ee6b1d69b669c04465d8c32eb6697
Taylor Blanton <matthewtblanton@gmail.com>
2025-11-05 22:14:39 -0600

    make the dr tulu tools a toolset again

--- src/custom_agents/dr_tulu/dr_tulu_toolset.py
+++ src/custom_agents/dr_tulu/dr_tulu_toolset.py
@@ -1,6 +1,7 @@
+from pydantic_ai.toolsets import FunctionToolset
 from src.custom_agents.dr_tulu.browse.browse_website_tool import browse_website_tool
 from src.custom_agents.dr_tulu.search.google_search_tool import google_search_tool
 from src.custom_agents.dr_tulu.search.snippet_search_tool import snippet_search_tool
 
 # TODO: See if we can use a WrapperToolset here https://ai.pydantic.dev/toolsets/#changing-tool-execution
-DR_TULU_TOOLS = [snippet_search_tool, google_search_tool, browse_website_tool]
+DR_TULU_TOOLS = FunctionToolset(tools=[snippet_search_tool, google_search_tool, browse_website_tool])

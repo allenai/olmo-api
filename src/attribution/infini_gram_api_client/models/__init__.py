cf22996e0e70ba499ec8b5199c7a10fc2856a7d2
Taylor Blanton <matthewtblanton@gmail.com>
2025-11-18 15:20:31 -0600

    update infini-gram-api-client (#424)
    Updated according to instructions in readme. There are no manual changes
    in this PR.
    
    This mostly gets us the updated index IDs. Changes other than that are
    either removed endpoints or formatting differences.

--- src/attribution/infini_gram_api_client/models/__init__.py
+++ src/attribution/infini_gram_api_client/models/__init__.py
@@ -6,15 +6,8 @@ from .attribution_request import AttributionRequest
 from .attribution_response import AttributionResponse
 from .attribution_span import AttributionSpan
 from .available_infini_gram_index_id import AvailableInfiniGramIndexId
-from .document import Document
-from .document_metadata import DocumentMetadata
-from .infini_gram_count_response import InfiniGramCountResponse
-from .infini_gram_document_response import InfiniGramDocumentResponse
-from .infini_gram_document_response_metadata import InfiniGramDocumentResponseMetadata
-from .infini_gram_documents_response import InfiniGramDocumentsResponse
 from .problem import Problem
 from .request_validation_error import RequestValidationError
-from .search_response import SearchResponse
 from .span_ranking_method import SpanRankingMethod
 from .validation_error import ValidationError
 
@@ -25,15 +18,8 @@ __all__ = (
     "AttributionResponse",
     "AttributionSpan",
     "AvailableInfiniGramIndexId",
-    "Document",
-    "DocumentMetadata",
-    "InfiniGramCountResponse",
-    "InfiniGramDocumentResponse",
-    "InfiniGramDocumentResponseMetadata",
-    "InfiniGramDocumentsResponse",
     "Problem",
     "RequestValidationError",
-    "SearchResponse",
     "SpanRankingMethod",
     "ValidationError",
 )

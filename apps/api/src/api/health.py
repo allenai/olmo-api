ea3419eb86086b1156a2b931bab887675f536cd7
Taylor Blanton <matthewtblanton@gmail.com>
2026-01-07 13:49:07 -0600

    check the DB connection in health check

--- apps/api/src/api/health.py
+++ apps/api/src/api/health.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter, status
+from sqlalchemy import select
+
+from api.db.sqlalchemy import SessionDependency
 
 health_router = APIRouter(prefix="/health")
 
 
 # Standard k8 health check route
 @health_router.get("/", status_code=status.HTTP_204_NO_CONTENT, include_in_schema=False)
-def health() -> None:
+async def health(session: SessionDependency) -> None:
+    async with session.begin():
+        await session.execute(select(1))
+
     return

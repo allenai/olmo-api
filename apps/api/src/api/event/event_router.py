f045fa019c547c63de77e516873dfe33f363d460
Byron Bischoff <bronzle@users.noreply.github.com>
2026-01-12 12:51:29 -0800

    V5 event (#474)
    ## Description:
    
    Simple event logger.
    
    Is this sufficient for the logging? (format?)
    
    
    closes: https://github.com/allenai/playground-issues-repo/issues/902

---
+++ apps/api/src/api/event/event_router.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Any
+
+import structlog
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+event_router = APIRouter()
+
+logger = structlog.getLogger()
+
+
+class Event(BaseModel):
+    occurred: datetime
+    type: str
+    details: dict[str, Any]
+
+
+# Log events from the UI
+# Using post with path instead of a route prefix to prevent unnecessary redirects
+@event_router.post("/event")
+def event(event: Event) -> None:
+    logger.info("UI Event", type=event.type, event_date=event.occurred, details=event.details)

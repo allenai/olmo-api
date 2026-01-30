7d166b7254ecd26c74cf63a680474379ce2c1e2e
Paul Laskowski <paull@allenai.org>
2026-01-29 17:05:28 -0800

    refactor user_service to remove async with self.session.begin()

--- apps/api/src/api/user/user_service.py
+++ apps/api/src/api/user/user_service.py
@@ -20,6 +20,7 @@ class UpsertUserRequest(APIInterface):
     media_collection_accepted_date: datetime | None = None
     media_collection_acceptance_revoked_date: datetime | None = None
 
+
 class UpsertUserResponse(APIInterface):
     id: str
     client: str
@@ -45,54 +46,55 @@ class UserService:
         request: UpsertUserRequest,
         token: Token,
     ) -> UpsertUserResponse | None:
-        async with self.session.begin():
-            stmt = select(User).where(User.client == token.client)
-            result = await self.session.scalars(stmt)
-            user_to_update = result.one_or_none()
+        stmt = select(User).where(User.client == token.client)
+        result = await self.session.scalars(stmt)
+        user_to_update = result.one_or_none()
 
-            if user_to_update is not None:
-                if request.id is not None:
-                    user_to_update.id = request.id
-                if request.terms_accepted_date is not None:
-                    user_to_update.terms_accepted_date = request.terms_accepted_date
-                if request.acceptance_revoked_date is not None:
-                    user_to_update.acceptance_revoked_date = request.acceptance_revoked_date
-                if request.data_collection_accepted_date is not None:
-                    user_to_update.data_collection_accepted_date = request.data_collection_accepted_date
-                if request.data_collection_acceptance_revoked_date is not None:
-                    user_to_update.data_collection_acceptance_revoked_date = (
-                        request.data_collection_acceptance_revoked_date
-                    )
-                if request.media_collection_accepted_date is not None:
-                    user_to_update.media_collection_accepted_date = request.media_collection_accepted_date
-                if request.media_collection_acceptance_revoked_date is not None:
-                    user_to_update.media_collection_acceptance_revoked_date = (
-                        request.media_collection_acceptance_revoked_date
-                    )
+        if user_to_update is not None:
+            if request.id is not None:
+                user_to_update.id = request.id
+            if request.terms_accepted_date is not None:
+                user_to_update.terms_accepted_date = request.terms_accepted_date
+            if request.acceptance_revoked_date is not None:
+                user_to_update.acceptance_revoked_date = request.acceptance_revoked_date
+            if request.data_collection_accepted_date is not None:
+                user_to_update.data_collection_accepted_date = request.data_collection_accepted_date
+            if request.data_collection_acceptance_revoked_date is not None:
+                user_to_update.data_collection_acceptance_revoked_date = request.data_collection_acceptance_revoked_date
+            if request.media_collection_accepted_date is not None:
+                user_to_update.media_collection_accepted_date = request.media_collection_accepted_date
+            if request.media_collection_acceptance_revoked_date is not None:
+                user_to_update.media_collection_acceptance_revoked_date = (
+                    request.media_collection_acceptance_revoked_date
+                )
 
-                await self.session.flush()
-
-                return UpsertUserResponse.model_validate(user_to_update)
-
-            new_user = User(
-                client=token.client,
-                terms_accepted_date=request.terms_accepted_date or datetime.now().astimezone(UTC),
-                acceptance_revoked_date=request.acceptance_revoked_date,
-                data_collection_accepted_date=request.data_collection_accepted_date,
-                data_collection_acceptance_revoked_date=request.data_collection_acceptance_revoked_date,
-                media_collection_accepted_date=request.media_collection_accepted_date,
-                media_collection_acceptance_revoked_date=request.media_collection_acceptance_revoked_date,
-            )
-
-            self.session.add(new_user)
             await self.session.flush()
+            await self.session.commit()
+            await self.session.refresh(user_to_update)
 
-            # Create HubSpot contact for new non-anonymous users
-            should_create_contact = not token.is_anonymous_user
-            if should_create_contact and new_user:
-                await self.hubspot_service.create_contact()
+            return UpsertUserResponse.model_validate(user_to_update)
 
-            return UpsertUserResponse.model_validate(new_user)
+        new_user = User(
+            client=token.client,
+            terms_accepted_date=request.terms_accepted_date or datetime.now().astimezone(UTC),
+            acceptance_revoked_date=request.acceptance_revoked_date,
+            data_collection_accepted_date=request.data_collection_accepted_date,
+            data_collection_acceptance_revoked_date=request.data_collection_acceptance_revoked_date,
+            media_collection_accepted_date=request.media_collection_accepted_date,
+            media_collection_acceptance_revoked_date=request.media_collection_acceptance_revoked_date,
+        )
+
+        self.session.add(new_user)
+        await self.session.flush()
+        await self.session.commit()
+        await self.session.refresh(new_user)
+
+        # Create HubSpot contact for new non-anonymous users
+        should_create_contact = not token.is_anonymous_user
+        if should_create_contact and new_user:
+            await self.hubspot_service.create_contact()
+
+        return UpsertUserResponse.model_validate(new_user)
 
 
 UserServiceDependency = Annotated[UserService, Depends()]

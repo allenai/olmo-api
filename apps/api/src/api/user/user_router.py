4e34bfcf73e6bf9836f5655abede0e6523f0c140
Paul Laskowski <warmbowski@users.noreply.github.com>
2026-02-04 16:31:47 -0800

    migrate  /v3/user and /v3/migrate-user to /V5/user and /v5/user/migration (#490)
    migrate the flask app /v3/user and /v3/migrate-user endpoints to
    fast-api app as /V5/user and /v5/user/migration
    
    * small changes to request objects to remove client info that should be
    gotten from token
    * the user migration endpoint removes GCS customTime updates to remove
    the TTL as it doesn't seem to be set on upload anymore.
    * hubspot contact creation now also happens in user migration (for new
    auth users)

--- apps/api/src/api/user/user_router.py
+++ apps/api/src/api/user/user_router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from api.auth.auth_service import AuthServiceDependency
+from api.user.user_migration_service import UserMigrationRequest, UserMigrationResponse, UserMigrationServiceDependency
+from api.user.user_service import UpsertUserRequest, UpsertUserResponse, UserServiceDependency
 from api.user.user_who_am_i_service import UserWhoAmIServiceDependency
 from core.auth.authenticated_client import AuthenticatedClient
 
@@ -22,3 +24,40 @@ async def get_who_am_i(
     auth_client = await who_am_i_service.get_by_client(token)
 
     return auth_client
+
+
+@user_router.put("/")
+async def upsert_user(
+    request: UpsertUserRequest,
+    auth_service: AuthServiceDependency,
+    user_service: UserServiceDependency,
+) -> UpsertUserResponse | None:
+    """
+    Create or update a user record.
+
+    Accepts user info and creates or updates the user in the database.
+    For authenticated (non-anonymous) users, also creates a HubSpot contact.
+    """
+    token = auth_service.optional_auth()
+
+    user = await user_service.upsert_user(request, token)
+
+    return user
+
+
+@user_router.put("/migration")
+async def migrate_user(
+    request: UserMigrationRequest,
+    auth_service: AuthServiceDependency,
+    user_migration_service: UserMigrationServiceDependency,
+) -> UserMigrationResponse:
+    """
+    Migrates annonymous user data to the logged in user account.
+
+    Accepts an anonymous user ID.
+    """
+    token = auth_service.require_auth()
+
+    user = await user_migration_service.migrate_user_from_anonymous_user(request, token)
+
+    return user

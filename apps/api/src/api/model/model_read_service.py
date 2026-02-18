99a5d03c8e8f8fd90924f2aeeeae733e905864ce
Taylor Blanton <matthewtblanton@gmail.com>
2026-02-18 17:20:07 -0600

    make a shared model repository to share logic

--- apps/api/src/api/model/model_read_service.py
+++ apps/api/src/api/model/model_read_service.py
@@ -2,48 +2,35 @@ from typing import Annotated
 
 from fastapi import Depends
 from pydantic import TypeAdapter
-from sqlalchemy import select
-from sqlalchemy.orm import selectin_polymorphic
 
-from api.db.sqlalchemy_engine import SessionDependency
+from api.model.model_repository import ModelRepositoryDependency
 from api.model.model_response import AvailableTool, ModelListResponse, ModelResponse, ModelValidationContext
 from api.tools.tools_service import ToolsServiceDependency
-from db.models.model_config import FilesOnlyModelConfig, ModelConfig, MultiModalModelConfig
 
 
 class ModelReadService:
-    def __init__(self, session: SessionDependency, tools_service: ToolsServiceDependency):
-        self.session = session
+    def __init__(self, model_repository: ModelRepositoryDependency, tools_service: ToolsServiceDependency):
         self.tools_service = tools_service
+        self.model_repository = model_repository
 
     async def get_all(self, *, include_internal_models: bool = False) -> ModelListResponse:
-        async with self.session.begin():
-            polymorphic_loader_opt = selectin_polymorphic(
-                ModelConfig, [ModelConfig, MultiModalModelConfig, FilesOnlyModelConfig]
+        result = await self.model_repository.get_all(include_internal_models=include_internal_models)
+
+        model_validation_context = ModelValidationContext(should_show_internal_models=include_internal_models)
+
+        mapped_models = [
+            ModelResponse.model_validate(model, from_attributes=True, context=model_validation_context)
+            for model in result
+        ]
+
+        # Mutating the mapped models list here, would love to have a more elegant way of doing this
+        available_tool_list_type_adapter = TypeAdapter(list[AvailableTool])
+        for mapped_model in mapped_models:
+            mapped_model.root.available_tools = available_tool_list_type_adapter.validate_python(
+                await self.tools_service.get_available_tools(model=mapped_model.root)
             )
 
-            stmt = select(ModelConfig).options(polymorphic_loader_opt).order_by(ModelConfig.order.asc())
-
-            if not include_internal_models:
-                stmt = stmt.filter_by(internal=False)
-
-            result = await self.session.scalars(stmt)
-
-            model_validation_context = ModelValidationContext(should_show_internal_models=include_internal_models)
-
-            mapped_models = [
-                ModelResponse.model_validate(model, from_attributes=True, context=model_validation_context)
-                for model in result.all()
-            ]
-
-            # Mutating the mapped models list here, would love to have a more elegant way of doing this
-            available_tool_list_type_adapter = TypeAdapter(list[AvailableTool])
-            for mapped_model in mapped_models:
-                mapped_model.root.available_tools = available_tool_list_type_adapter.validate_python(
-                    await self.tools_service.get_available_tools(model=mapped_model.root)
-                )
-
-            return ModelListResponse.model_validate(mapped_models)
+        return ModelListResponse.model_validate(mapped_models)
 
 
 ModelReadServiceDependency = Annotated[ModelReadService, Depends()]
--- apps/api/src/api/model_config/admin/model_config_admin_read_service.py
+++ apps/api/src/api/model_config/admin/model_config_admin_read_service.py
@@ -1,42 +1,29 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy import select
-from sqlalchemy.orm import selectin_polymorphic
 
-from api.db.sqlalchemy_engine import SessionDependency
+from api.model.model_repository import ModelRepositoryDependency
 from api.model_config.model_config_response import ModelConfigListResponse, ModelConfigResponse
-from db.models.model_config import ModelConfig, MultiModalModelConfig
 
 
 class ModelConfigAdminReadService:
-    def __init__(self, session: SessionDependency):
-        self.session = session
+    def __init__(self, model_repository: ModelRepositoryDependency):
+        self.model_repository = model_repository
 
     async def get_all(self) -> ModelConfigListResponse:
-        async with self.session.begin():
-            polymorphic_loader_opt = selectin_polymorphic(ModelConfig, [ModelConfig, MultiModalModelConfig])
+        result = await self.model_repository.get_all()
 
-            stmt = select(ModelConfig).options(polymorphic_loader_opt).order_by(ModelConfig.order.asc())
+        processed_results = [ModelConfigResponse.model_validate(model) for model in result]
 
-            result = await self.session.scalars(stmt)
-
-            processed_results = [ModelConfigResponse.model_validate(model) for model in result.all()]
-
-            return ModelConfigListResponse.model_validate(processed_results)
+        return ModelConfigListResponse.model_validate(processed_results)
 
     async def get_one(self, model_id: str) -> ModelConfigResponse | None:
-        async with self.session.begin():
-            polymorphic_loader_opt = selectin_polymorphic(ModelConfig, [ModelConfig, MultiModalModelConfig])
-            stmt = select(ModelConfig).options(polymorphic_loader_opt).where(ModelConfig.id == model_id)
+        model_config = await self.model_repository.get_one(model_id)
 
-            result = await self.session.scalars(stmt)
-            model_config = result.one_or_none()
+        if model_config is None:
+            return None
 
-            if model_config is None:
-                return None
-
-            return ModelConfigResponse.model_validate(model_config)
+        return ModelConfigResponse.model_validate(model_config)
 
 
 ModelConfigAdminReadServiceDependency = Annotated[ModelConfigAdminReadService, Depends()]

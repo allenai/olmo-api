da2ce7ae7d2704b0b9b46c646a03faa115b1b381
Taylor Blanton <matthewtblanton@gmail.com>
2026-02-20 11:23:43 -0600

    lint fix

--- apps/api/src/api/model_config/admin/model_config_admin_read_service.py
+++ apps/api/src/api/model_config/admin/model_config_admin_read_service.py
@@ -1,12 +1,11 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy.orm import selectin_polymorphic
 
 from api.db.sqlalchemy_engine import SessionDependency
 from api.model.model_query import base_model_config_select
 from api.model_config.model_config_response import ModelConfigListResponse, ModelConfigResponse
-from db.models.model_config import ModelConfig, MultiModalModelConfig
+from db.models.model_config import ModelConfig
 
 
 class ModelConfigAdminReadService:
@@ -25,7 +24,6 @@ class ModelConfigAdminReadService:
 
     async def get_one(self, model_id: str) -> ModelConfigResponse | None:
         async with self.session.begin():
-            polymorphic_loader_opt = selectin_polymorphic(ModelConfig, [ModelConfig, MultiModalModelConfig])
             stmt = base_model_config_select.where(ModelConfig.id == model_id)
 
             result = await self.session.scalars(stmt)

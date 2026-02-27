9997011cadfcaf7b4dae9dfe7b8e02689a684fe8
Taylor Blanton <matthewtblanton@gmail.com>
2026-02-27 12:40:51 -0600

    tell ruff to ignore assert S issues in e2e folder

--- apps/api/e2e/_util.py
+++ apps/api/e2e/_util.py
@@ -4,6 +4,4 @@ from httpx import Response
 
 
 def assert_ok_response(response: Response):
-    assert response.status_code == HTTPStatus.OK, (  # noqa: S101
-        f"{response.url} responded with an non-success: {response.text}"
-    )
+    assert response.status_code == HTTPStatus.OK, f"{response.url} responded with an non-success: {response.text}"

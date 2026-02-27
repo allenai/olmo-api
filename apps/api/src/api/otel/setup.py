9ba84bfcb2390553110b11d6811516b518728f65
Taylor Blanton <matthewtblanton@gmail.com>
2026-02-27 13:10:33 -0600

    updating otel setup based on OTEL Python docs

--- apps/api/src/api/otel/setup.py
+++ apps/api/src/api/otel/setup.py
@@ -1,30 +1,23 @@
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import SpanProcessor, TracerProvider
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, SimpleSpanProcessor
 from opentelemetry.trace import set_tracer_provider
 
 from api.config import settings
 
 
-class CustomAttributeSpanProcessor(SpanProcessor):
-    # ignoring formatting on this because its inherited (even though its probably safe to change the signature)
-    def on_start(self, span, parent_context=None):  # noqa: ARG002, PLR6301
-        span.set_attributes({
-            "service.name": settings.OTEL_SERVICE_NAME,
-        })
-
-    def on_end(self, span):
-        pass
-
-
 def setup_otel() -> None:
-    tracer_provider = TracerProvider()
-
-    tracer_provider.add_span_processor(CustomAttributeSpanProcessor())
+    resource = Resource.create(attributes={SERVICE_NAME: settings.OTEL_SERVICE_NAME})
+    tracer_provider = TracerProvider(resource=resource)
 
     if settings.OTEL_COLLECTOR_TYPE == "local":
-        tracer_provider.add_span_processor(span_processor=SimpleSpanProcessor(OTLPSpanExporter()))
+        tracer_provider.add_span_processor(
+            span_processor=SimpleSpanProcessor(
+                OTLPSpanExporter(endpoint=settings.OTEL_EXPORTER_OTLP_ENDPOINT + "/v1/traces")
+            )
+        )
     elif settings.OTEL_COLLECTOR_TYPE == "cloud":
         tracer_provider.add_span_processor(
             BatchSpanProcessor(CloudTraceSpanExporter(project_id=settings.OTEL_GCP_PROJECT_ID))

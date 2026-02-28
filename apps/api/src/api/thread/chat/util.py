5c832cb5e9903f0eeed04b7c042c381de5bdb4c8
Taylor Blanton <matthewtblanton@gmail.com>
2026-02-27 18:08:28 -0600

    don't change messages with existing children

--- apps/api/src/api/thread/chat/util.py
+++ apps/api/src/api/thread/chat/util.py
@@ -6,7 +6,8 @@ from db.models.message import Message
 def attach_message_children(msg_chain: Sequence[Message]):
     for i, msg in enumerate(msg_chain):
         next_msg = msg_chain[i + 1] if i < len(msg_chain) - 1 else None
-        if next_msg:
+        # This will need to change if we allow threading
+        if next_msg and not next_msg.children:
             msg.children = [next_msg]
             next_msg.parent = msg.id
 

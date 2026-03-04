8ae7295d910bd2fd6f618b555c8b330b718e0745
Byron Bischoff <byronb@allenai.org>
2026-03-04 15:27:46 -0800

    fix tests

--- apps/api/e2e/test_chat.py
+++ apps/api/e2e/test_chat.py
@@ -65,12 +65,12 @@ async def test_calls_tools(client: AsyncClient, auth_user: AuthenticatedClient, 
     assert len(lines) == 6
     StreamStartChunk.model_validate(lines[0])
     starting_thread = StartThreadChunk.model_validate(lines[1])
-    tool_call_message = AddMessageChunk.model_validate(lines[2])
+    AddMessageChunk.model_validate(lines[2])
+    tool_call_chunk = ToolCallChunk.model_validate(lines[3])
     finished_thread = FinalThreadChunk.model_validate(lines[-2])
     StreamEndChunk.model_validate(lines[-1])
 
-    assert tool_call_message.messages[0].tool_calls, "There were no tool calls on the intended tool call message"
-    assert tool_call_message.messages[0].tool_calls[0].tool_name == tool_name
+    assert tool_call_chunk.tool_name == tool_name
     assert len(starting_thread.messages) == 2
     assert finished_thread.id == starting_thread.id
     assert len(finished_thread.messages) == 3
@@ -80,6 +80,7 @@ async def test_calls_tools(client: AsyncClient, auth_user: AuthenticatedClient, 
     assert finished_thread.messages[2].role == Role.Assistant
     assert finished_thread.messages[2].tool_calls
     assert len(finished_thread.messages[2].tool_calls) == 1
+    assert finished_thread.messages[2].tool_calls[0].tool_name == tool_name
 
     async with db_session() as session, session.begin():
         message_query = (
@@ -134,20 +135,18 @@ async def test_tool_call_user_response(client: AsyncClient, auth_user: Authentic
     assert len(lines) == 6
     StreamStartChunk.model_validate(lines[0])
     StartThreadChunk.model_validate(lines[1])
-    tool_call_message = AddMessageChunk.model_validate(lines[2])
+    AddMessageChunk.model_validate(lines[2])
+    tool_call_chunk = ToolCallChunk.model_validate(lines[3])
     finished_thread = FinalThreadChunk.model_validate(lines[-2])
     StreamEndChunk.model_validate(lines[-1])
 
-    assert tool_call_message.messages[0].tool_calls
-
-    tool_call_id = tool_call_message.messages[0].tool_calls[0].tool_call_id
 
     tool_request = ToolResponseChatRequest(
         content="Sunny",
         model="test-model",
         enable_tool_calling=True,
         parent=finished_thread.messages[2].id,
-        tool_call_id=tool_call_id,
+        tool_call_id=tool_call_chunk.tool_call_id,
     ).model_dump(exclude_none=True, exclude_computed_fields=True)
 
     tool_request["toolDefinitions"] = tool_definitions
@@ -165,7 +164,7 @@ async def test_tool_call_user_response(client: AsyncClient, auth_user: Authentic
     StreamEndChunk.model_validate(lines[-1])
 
     assert tool_response_chunk.messages[0].tool_calls, "There were no tool calls in the tool result response"
-    assert tool_response_chunk.messages[0].tool_calls[0].tool_call_id == tool_call_id
+    assert tool_response_chunk.messages[0].tool_calls[0].tool_call_id == tool_call_chunk.tool_call_id
     assert tool_response_chunk.messages[0].content == "Sunny"
 
     assert len(final_therad_chunk.messages) == 2

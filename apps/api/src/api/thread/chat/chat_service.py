5cf631574b5fe8b49267b2d329ab3d5285d81c33
Taylor Blanton <matthewtblanton@gmail.com>
2026-03-06 14:12:23 -0600

    make the file upload blocking

--- apps/api/src/api/thread/chat/chat_service.py
+++ apps/api/src/api/thread/chat/chat_service.py
@@ -364,7 +364,6 @@ class ChatService:
         )
         
         if request.files:
-            # TODO: Figure out if parent_message_id is correct or if we need something like user_message_id
             await self.file_upload_service.upload_request_files(parent_message_id, root_message_id, request.files)
 
         toolsets = self._get_toolsets(model, request.tool_definitions, mcp_tools=request.selected_tools)

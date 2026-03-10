aff6edb2c7f440be2ac23e73e54fa3477d35e831
Byron Bischoff <byronb@allenai.org>
2026-03-10 15:50:05 -0700

    use DI for the safety checker

--- apps/api/src/api/thread/chat/safety/video_safety_checker_service.py
+++ apps/api/src/api/thread/chat/safety/video_safety_checker_service.py
@@ -23,6 +23,9 @@ def get_video_safety_checker() -> SafetyChecker:
     return GoogleVideoIntelligenceSafetyChecker()
 
 
+GoogleVideoSafetyCheckerDependecy = Annotated[SafetyChecker, Depends(get_video_safety_checker)]
+
+
 def generate_random_filename(original_filename: str) -> str:
     extension = Path(original_filename).suffix
     random_name = f"{uuid.uuid4().hex}{extension}"
@@ -30,29 +33,28 @@ def generate_random_filename(original_filename: str) -> str:
 
 
 class VideoSafetyCheckerService:
-    def __init__(self, cloud_storage: GoogleCloudStorageDependency):
-        self.cloud_storage = cloud_storage
+    def __init__(self, cloud_storage: GoogleCloudStorageDependency, checker: GoogleVideoSafetyCheckerDependecy):
+        self._cloud_storage = cloud_storage
+        self._checker = checker
 
     @tracer.start_as_current_span("VideoSafetyCheckerService/check_video_safety")
     async def check_video_safety(self, files: Sequence[UploadFile], message_id: str) -> bool:
-        checker = get_video_safety_checker()
-
         for file in files:
             filename = ""
             try:
                 filename = generate_random_filename(file.filename or ".unknown")
 
                 await file.seek(0)
-                response = await self.cloud_storage.upload_content(
+                response = await self._cloud_storage.upload_content(
                     filename=filename, file_data=await file.read(), bucket_name=settings.SAFTEY_GCS_UPLOAD_BUCKET
                 )
                 request = SafetyCheckRequest(message_id=message_id, content=response.storage_path, name=filename)
 
-                result = await checker.check_request(request)
+                result = await self._checker.check_request(request)
 
                 # this handles the file deletion for the inline strategy
                 if not result.is_safe():
-                    await self.cloud_storage.delete_file(
+                    await self._cloud_storage.delete_file(
                         response.storage_path, bucket_name=settings.SAFTEY_GCS_UPLOAD_BUCKET
                     )
                     return False

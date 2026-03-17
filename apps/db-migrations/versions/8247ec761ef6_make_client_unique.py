4d65358dc9fe7fb1d5534119a129c083689e7e6c
Byron Bischoff <byronb@allenai.org>
2026-03-16 18:17:58 -0700

    add constraint on client

---
+++ apps/db-migrations/versions/8247ec761ef6_make_client_unique.py
@@ -0,0 +1,29 @@
+"""make client unique
+
+Revision ID: 8247ec761ef6
+Revises: 20c0085a0629
+Create Date: 2026-03-16 16:18:25.715359
+
+"""
+
+from collections.abc import Sequence
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "8247ec761ef6"
+down_revision: str | None = "20c0085a0629"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.drop_index("client_idx", table_name="olmo_user")
+    op.create_index(op.f("client_idx"), "olmo_user", ["client"], unique=True)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_index(op.f("client_idx"), table_name="olmo_user")
+    op.create_index("client_idx", "olmo_user", ["client"], unique=False)
--- packages/db/src/db/models/user.py
+++ packages/db/src/db/models/user.py
@@ -13,7 +13,7 @@ class User(Base, kw_only=True):
     __table_args__ = (PrimaryKeyConstraint("id", name="olmo_user_pkey"),)
 
     id: Mapped[str] = mapped_column(Text, primary_key=True, default_factory=new_id_generator("user"))
-    client: Mapped[str] = mapped_column(Text, nullable=False, index=True)
+    client: Mapped[str] = mapped_column(Text, nullable=False, index=True, unique=True)
     terms_accepted_date: Mapped[datetime.datetime]
     acceptance_revoked_date: Mapped[datetime.datetime | None]
     data_collection_accepted_date: Mapped[datetime.datetime | None]
